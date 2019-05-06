2c51a6eca5e823e7b9d75d916ee7e5ba00da371e
toni <toni@galvintec.com>
2019-05-06 11:23:02 +0200

    Correct order for JailComponent imports

--- apps/sga/src/app/jail/jail.module.ts
+++ apps/sga/src/app/jail/jail.module.ts
@@ -1,29 +1,27 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { MatTableModule, MatCheckboxModule } from '@angular/material';
 import { CdkTableModule } from '@angular/cdk/table';
-import { StoreComponent } from './store/store.component';
-import { UpdateComponent } from './update/update.component';
-import {CommonUiCrudModule, ListComponent} from '@suite/common/ui/crud';
-import { FormsModule } from '@angular/forms';
 import { JailRoutingModule } from './jail-routing.module';
 import { JailComponent } from './jail.component';
+import { StoreComponent } from './store/store.component';
+import { UpdateComponent } from './update/update.component';
+import {CommonUiCrudModule} from '@suite/common/ui/crud';
 
 
 @NgModule({
   declarations: [JailComponent, StoreComponent, UpdateComponent],
   imports: [
     CommonModule,
-    JailRoutingModule,
-    FormsModule,
     ReactiveFormsModule,
     IonicModule,
     MatTableModule,
     MatCheckboxModule,
+    JailRoutingModule,
     CommonUiCrudModule,
-    CdkTableModule,
+    CdkTableModule
   ], entryComponents: [
     StoreComponent
   ]
