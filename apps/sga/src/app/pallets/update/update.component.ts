417b7a93bd78f5920b4fce966ad6dfdfa6613260
toni <toni@galvintec.com>
2019-05-07 09:51:57 +0200

    Pallets component has been added

---
+++ apps/sga/src/app/pallets/update/update.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Validators } from '@angular/forms';
+import { COLLECTIONS } from 'config/base';
+
+@Component({
+  selector: 'suite-update',
+  templateUrl: './update.component.html',
+  styleUrls: ['./update.component.scss']
+})
+export class UpdateComponent implements OnInit {
+  formBuilderDataInputs = {
+    reference: ['', [Validators.required, Validators.pattern('/^[P][0-9]{4}$')]]
+  };
+  formBuilderTemplateInputs = [
+    {
+      name: 'reference',
+      label: 'Referencia',
+      type: 'text',
+      icon: { type: 'ionic', name: 'filing'}
+    }
+  ];
+  title = 'Actualizar Palet';
+  apiEndpoint = COLLECTIONS.find(collection => collection.name === 'Pallets')
+    .name;
+
+  redirectTo = '/pallets/list';
+
+  constructor() {}
+
+  ngOnInit() {}
+}
