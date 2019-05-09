ab2c560d4d8cbfeb40dcf05fa5e1935952ceae5b
toni <toni@galvintec.com>
2019-05-09 07:47:05 +0200

    Warehouse model has been added

---
+++ libs/services/src/models/endpoints/Warehouse.ts
@@ -0,0 +1,29 @@
+export namespace WarehouseModel {
+  export interface Warehouse {
+    id?: number;
+    name: string;
+    description: string;
+    reference: string;
+    is_store: boolean;
+    is_main: boolean;
+    groupId: number;
+  }
+
+  export interface ResponseIndex {
+    data: Warehouse[];
+    message: string;
+    code: number;
+  }
+
+  export interface ResponseShow {
+    data: Warehouse;
+  }
+
+  export interface ErrorResponseShow {
+    statusCode: number;
+    status: number;
+    code: number;
+    message: string;
+    name: string;
+  }
+}
