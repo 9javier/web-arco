6852bbbf9cf0cbc67b7cd36e5c749afb6cf70021
toni <toni@galvintec.com>
2019-05-09 10:02:54 +0200

    Merge branch 'dev' of C:\Users\pc\Documents\IdeaProjects\Krack 2020\SGA + App Logistica Krack with conflicts.

--- apps/sga/src/app/roles/roles.module.ts
+++ apps/sga/src/app/roles/roles.module.ts
@@ -9,7 +9,8 @@ import { RolesComponent } from './roles.component';
 import { StoreComponent } from './store/store.component';
 import { UpdateComponent } from './update/update.component';
 import { CommonUiCrudModule } from '@suite/common/ui/crud';
-import { AssignModule } from '../assign/assign.module';
+import {AssignModule} from "../assign/assign.module";
+
 
 @NgModule({
   declarations: [RolesComponent, StoreComponent, UpdateComponent],
@@ -23,6 +24,9 @@ import { AssignModule } from '../assign/assign.module';
     CdkTableModule,
     CommonUiCrudModule,
     AssignModule
+  ],
+  entryComponents: [
+    StoreComponent
   ]
 })
 export class RolesModule {}
