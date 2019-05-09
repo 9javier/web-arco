66080668da822c0ff19bd0b2505fdcc45c7cbe06
toni <toni@galvintec.com>
2019-05-09 08:10:35 +0200

    Print button has been added in the table of jails and pallets

--- apps/sga/src/app/pallets/pallets.component.ts
+++ apps/sga/src/app/pallets/pallets.component.ts
@@ -10,7 +10,7 @@ import {ModalController} from "@ionic/angular";
 
 export class PalletsComponent implements OnInit {
   public title = 'Palets';
-  public displayedColumns: string[] = ['id', 'reference', 'select'];
+  public displayedColumns: string[] = ['select', 'id', 'reference', 'buttons-print' ];
   public columns: string[] = ['id', 'reference'];
   public apiEndpoint = COLLECTIONS.find(collection => collection.name === 'Pallets').name;
   public routePath = '/pallets';
