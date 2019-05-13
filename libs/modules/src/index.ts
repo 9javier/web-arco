335e7f2b078fda7e83de1a25beef2783548e89bd
Freddy Hidalgo <hifreddytech@gmail.com>
2019-05-12 21:37:04 -0600

    add modules in libs(new place to shared work)

--- libs/modules/src/index.ts
+++ libs/modules/src/index.ts
@@ -1,27 +1,3 @@
-export * from './lib/services.module';
-
-/** Authentication */
-export * from './lib/endpoint/authentication/authentication.service';
-/** OAuth */
-export * from './models/endpoints/OAuth2';
-export * from './lib/endpoint/oauth2/oauth2.service';
-/** Permissions */
-export * from './models/endpoints/Permissions';
-export * from './lib/endpoint/permissions/permissions.service';
-
-/** Users */
-export * from './models/endpoints/User';
-export * from './lib/endpoint/users/users.service';
-
-/** Roles */
-export * from './models/endpoints/Rol';
-export * from './lib/endpoint/roles/roles.service';
-
-/** ACL */
-export * from './models/endpoints/ACL';
-export * from './lib/endpoint/acl/acl.service';
-
-
 
 /** Jail */
 export * from './jail/jail.module';
