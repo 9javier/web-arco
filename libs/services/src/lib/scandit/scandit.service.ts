7132c905945057453bee6e5585a3d73e7014164c
Luis Álvarez <luis.albeos@gmail.com>
2019-05-15 07:14:22 +0200

    Remove unused code

--- libs/services/src/lib/scandit/scandit.service.ts
+++ libs/services/src/lib/scandit/scandit.service.ts
@@ -26,13 +26,6 @@ export class ScanditService {
     Scandit.License.setAppKey(SCANDIT_API_KEY);
   }
 
-  scanReferences() {
-    GScandit.matrix_bubble((response) => {
-      console.log("TEST::GScandit response", response);
-      GScandit.setDataReference(response.barcode.id, response.barcode.data, response.barcode.data, '10,99', [{attribute_name:'40', color: 'Azul'}], false, "Nothing", "Nothing");
-    }, '', '', 1);
-  }
-
   positioning() {
 
     let positionsScanning = [];
