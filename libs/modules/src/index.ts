4304d79004b256a0ca949daef047a544f4cc8292
toni <toni@galvintec.com>
2019-05-20 12:56:40 +0200

    Correct imports for moved modules

--- libs/modules/src/index.ts
+++ libs/modules/src/index.ts
@@ -1,6 +1,23 @@
 
 export * from './scanner-configuration/scanner-configuration.component';
 export * from './scanner-configuration/scanner-configuration.module';
+
 export * from './locations/locations.component';
 export * from './locations/locations.module';
 
+export * from './jail/jail.component';
+export * from './jail/jail.module';
+
+export * from './pallets/pallets.component';
+export * from './pallets/pallets.module';
+
+export * from './group-to-warehouse/group-to-warehouse.component';
+export * from './group-to-warehouse/group-to-warehouse.module';
+
+export * from './groups/groups.component';
+export * from './groups/groups.module';
+
+export * from './assign/assign.module';
+
+export * from './products/products.component';
+export * from './products/products.module';
