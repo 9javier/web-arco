eee50b6a550e0c00df1bf90a1aba88dd7842630e
Luis Álvarez <luis.albeos@gmail.com>
2019-05-23 12:16:51 +0200

    Change printer MAC address for Krack

--- config/base.ts
+++ config/base.ts
@@ -67,6 +67,6 @@ export namespace AppInfo {
 
 // TODO move printer MAC address to app's local configuration
 /** Krack's - Rafa - test printer */
-export const PRINTER_MAC: string = 'AC:3F:A4:18:25:FB';
+export const PRINTER_MAC: string = 'AC:3F:A4:11:06:F1';
 /** Galvintec's test printer */
 //export const PRINTER_MAC: string = 'AC:3F:A4:BD:F1:6F';
