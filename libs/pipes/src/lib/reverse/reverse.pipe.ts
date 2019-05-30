83c2c4e2d4d1f304b7ea852fea8bfdf761830e6f
Áxel <axel@galvintec.com>
2019-05-30 02:24:26 +0200

    Fix runtime error when applying reverse pipe to empty value.

--- libs/pipes/src/lib/reverse/reverse.pipe.ts
+++ libs/pipes/src/lib/reverse/reverse.pipe.ts
@@ -4,6 +4,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 export class ReversePipe implements PipeTransform {
   transform(value) {
-    return value.slice().reverse();
+    return value && value.slice().reverse();
   }
 }
