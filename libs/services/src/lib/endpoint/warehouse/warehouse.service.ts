89bb5c19d73e9e277798e5b0033bd13a0f646410
Áxel <axel@galvintec.com>
2019-06-06 14:59:52 +0200

    Get the full warehouse, racks and containers list from server with a single new method call. Fix terrible slowness after login.

--- libs/services/src/lib/endpoint/warehouse/warehouse.service.ts
+++ libs/services/src/lib/endpoint/warehouse/warehouse.service.ts
@@ -11,6 +11,7 @@ import {map, switchMap} from 'rxjs/operators';
 
 const PATH_GET_WAREHOUSE_MAIN: string = PATH('Warehouses', 'Main');
 const PATH_GET_WAREHOUSE_INDEX: string = PATH('Warehouses', 'Index');
+const PATH_GET_WAREHOUSE_FULL_INDEX: string = PATH('Warehouses', 'Full');
 
 import { environment } from '../../../environments/environment';
 
@@ -64,6 +65,15 @@ export class WarehouseService {
     });
   }
 
+  async getFullIndex() {
+    const currentToken = await this.auth.getCurrentToken();
+    const headers = new HttpHeaders({Authorization: currentToken});
+    return this.http.get(PATH_GET_WAREHOUSE_FULL_INDEX, {
+      headers: headers,
+      observe: 'response'
+    });
+  }
+
   loadWarehousesWithRacks() {
     this.getIndex()
       .then((data: Observable<HttpResponse<WarehouseModel.ResponseIndex>>) => {
@@ -82,70 +92,56 @@ export class WarehouseService {
     this.listColumns = {};
     this.listReferences = {};
 
-    this.getIndex()
+    this.getFullIndex()
       .then((data: Observable<HttpResponse<any>>) => {
         data.subscribe((res: HttpResponse<any>) => {
           res.body.data.forEach(warehouse => {
             this.listWarehouses.push({id: warehouse.id, value: warehouse.name, has_racks: warehouse.has_racks });
-
-            this.hallsService
-              .getIndex(warehouse.id)
-              .then((dataHall: Observable<HttpResponse<HallModel.ResponseIndex>>) => {
-                dataHall.subscribe((resHall: HttpResponse<HallModel.ResponseIndex>) => {
-                  resHall.body.data.forEach(hall => {
-                    if (typeof this.listHalls[warehouse.id] == 'undefined') {
-                      this.listHalls[warehouse.id] = [];
-                    }
-                    this.listHalls[warehouse.id].push({id: hall.id, value: hall.hall});
-
-                    this.hallsService
-                      .getShow(hall.id)
-                      .then((dataContainer: Observable<HttpResponse<HallModel.ResponseShow>>) => {
-                        dataContainer.subscribe((resContainer: HttpResponse<HallModel.ResponseShow>) => {
-                          let lastRow = 0;
-                          resContainer.body.data.forEach(container => {
-                            if (typeof this.listRows[warehouse.id] == 'undefined') {
-                              this.listRows[warehouse.id] = {};
-                            }
-                            if (typeof this.listColumns[warehouse.id] == 'undefined') {
-                              this.listColumns[warehouse.id] = {};
-                            }
-                            if (typeof this.listReferences[warehouse.id] == 'undefined') {
-                              this.listReferences[warehouse.id] = {};
-                            }
-                            if (typeof this.listColumns[warehouse.id] == 'undefined') {
-                              this.listColumns[warehouse.id] = {};
-                            }
-                            if (typeof this.listRows[warehouse.id][hall.id] == 'undefined') {
-                              this.listRows[warehouse.id][hall.id] = [];
-                            }
-                            if (typeof this.listColumns[warehouse.id][hall.id] == 'undefined') {
-                              this.listColumns[warehouse.id][hall.id] = [];
-                            }
-                            if (typeof this.listReferences[warehouse.id][hall.id] == 'undefined') {
-                              this.listReferences[warehouse.id][hall.id] = [];
-                            }
-                            if (typeof this.listColumns[warehouse.id][hall.id][container.row] == 'undefined') {
-                              this.listColumns[warehouse.id][hall.id][container.row] = [];
-                            }
-                            if (typeof this.listReferences[warehouse.id][hall.id][container.row] == 'undefined') {
-                              this.listReferences[warehouse.id][hall.id][container.row] = [];
-                            }
-                            if (typeof this.listReferences[warehouse.id][hall.id][container.row][container.column] == 'undefined') {
-                              this.listReferences[warehouse.id][hall.id][container.row][container.column] = [];
-                            }
-                            if (container.row != lastRow) {
-                              this.listRows[warehouse.id][hall.id].push({id: container.id, value: container.row, row: container.row});
-                              lastRow = container.row;
-                            }
-                            this.listColumns[warehouse.id][hall.id][container.row].push({id: container.id, value: container.column, column: container.column});
-                            this.listReferences[warehouse.id][hall.id][container.row][container.column] = container.reference;
-                          });
-                        });
-                      });
-                  });
-                });
+            warehouse.racks.forEach(hall => {
+              if (typeof this.listHalls[warehouse.id] == 'undefined') {
+                this.listHalls[warehouse.id] = [];
+              }
+              this.listHalls[warehouse.id].push({id: hall.id, value: hall.hall});
+              let lastRow = 0;
+              hall.containers.forEach(container => {
+                if (typeof this.listRows[warehouse.id] == 'undefined') {
+                  this.listRows[warehouse.id] = {};
+                }
+                if (typeof this.listColumns[warehouse.id] == 'undefined') {
+                  this.listColumns[warehouse.id] = {};
+                }
+                if (typeof this.listReferences[warehouse.id] == 'undefined') {
+                  this.listReferences[warehouse.id] = {};
+                }
+                if (typeof this.listColumns[warehouse.id] == 'undefined') {
+                  this.listColumns[warehouse.id] = {};
+                }
+                if (typeof this.listRows[warehouse.id][hall.id] == 'undefined') {
+                  this.listRows[warehouse.id][hall.id] = [];
+                }
+                if (typeof this.listColumns[warehouse.id][hall.id] == 'undefined') {
+                  this.listColumns[warehouse.id][hall.id] = [];
+                }
+                if (typeof this.listReferences[warehouse.id][hall.id] == 'undefined') {
+                  this.listReferences[warehouse.id][hall.id] = [];
+                }
+                if (typeof this.listColumns[warehouse.id][hall.id][container.row] == 'undefined') {
+                  this.listColumns[warehouse.id][hall.id][container.row] = [];
+                }
+                if (typeof this.listReferences[warehouse.id][hall.id][container.row] == 'undefined') {
+                  this.listReferences[warehouse.id][hall.id][container.row] = [];
+                }
+                if (typeof this.listReferences[warehouse.id][hall.id][container.row][container.column] == 'undefined') {
+                  this.listReferences[warehouse.id][hall.id][container.row][container.column] = [];
+                }
+                if (container.row != lastRow) {
+                  this.listRows[warehouse.id][hall.id].push({id: container.id, value: container.row, row: container.row});
+                  lastRow = container.row;
+                }
+                this.listColumns[warehouse.id][hall.id][container.row].push({id: container.id, value: container.column, column: container.column});
+                this.listReferences[warehouse.id][hall.id][container.row][container.column] = container.reference;
               });
+            });
           });
         });
       });
