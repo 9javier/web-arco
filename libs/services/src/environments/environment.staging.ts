8f154de2ac3413fb53f58e13f1863ddea4bd1676
Áxel <axel@galvintec.com>
2019-06-12 16:33:28 +0200

    Add staging configuration to AGIL. Properly rename files for more standarization.

--- libs/services/src/environments/environment.staging.ts
+++ libs/services/src/environments/environment.staging.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: true,
-  apiBase: 'http://192.168.0.36:8080/api',
+  production: false,
+  apiBase: 'http://192.168.254.109/api',
   client_id:"krack-client-sga",
   client_secret:"fGx4=yU-j4^jAAjZtV+YTDsm-@R$HAK3"
 };
