4512d6735d4c7d0a62fdeae5182fd741d054f75e
Áxel <axel@galvintec.com>
2019-06-13 13:36:06 +0200

    Run staging apk with right resources.

--- _dev/android/run_al_staging.bat
+++ _dev/android/run_al_staging.bat
@@ -1,8 +1,35 @@
 pushd %~dp0\..\..
 call ionic cordova plugin remove com.mirasense.scanditsdk.plugin
 call ionic cordova plugin add scandit\
+copy /Y apps\al\src\environments\environment.staging.ts apps\al\src\environments\environment.ts
 copy /Y libs\services\src\environments\environment.staging.ts libs\services\src\environments\environment.ts
+copy /Y config.staging.xml config.xml
+copy /Y resources.dev\icon.png resources\icon.png
+copy /Y resources.dev\android\icon\drawable-hdpi-icon.png resources\android\icon\drawable-hdpi-icon.png
+copy /Y resources.dev\android\icon\drawable-ldpi-icon.png resources\android\icon\drawable-ldpi-icon.png
+copy /Y resources.dev\android\icon\drawable-mdpi-icon.png resources\android\icon\drawable-mdpi-icon.png
+copy /Y resources.dev\android\icon\drawable-xhdpi-icon.png resources\android\icon\drawable-xhdpi-icon.png
+copy /Y resources.dev\android\icon\drawable-xxhdpi-icon.png resources\android\icon\drawable-xxhdpi-icon.png
+copy /Y resources.dev\android\icon\drawable-xxxhdpi-icon.png resources\android\icon\drawable-xxxhdpi-icon.png
 call ionic cordova run android --device --project al
-copy /Y libs\services\src\environments\environment.ts libs\services\src\environments\environment.prod.ts
+copy /Y apps\al\src\environments\environment.ts apps\al\src\environments\environment.staging.ts
+copy /Y libs\services\src\environments\environment.ts libs\services\src\environments\environment.staging.ts
+copy /Y config.xml config.staging.xml
+copy /Y resources\icon.png resources.dev\icon.png
+copy /Y resources\android\icon\drawable-hdpi-icon.png resources.dev\android\icon\drawable-hdpi-icon.png
+copy /Y resources\android\icon\drawable-ldpi-icon.png resources.dev\android\icon\drawable-ldpi-icon.png
+copy /Y resources\android\icon\drawable-mdpi-icon.png resources.dev\android\icon\drawable-mdpi-icon.png
+copy /Y resources\android\icon\drawable-xhdpi-icon.png resources.dev\android\icon\drawable-xhdpi-icon.png
+copy /Y resources\android\icon\drawable-xxhdpi-icon.png resources.dev\android\icon\drawable-xxhdpi-icon.png
+copy /Y resources\android\icon\drawable-xxxhdpi-icon.png resources.dev\android\icon\drawable-xxxhdpi-icon.png
+git checkout apps\al\src\environments\environment.ts
 git checkout libs\services\src\environments\environment.ts
+git checkout config.xml
+git checkout resources\icon.png
+git checkout resources\android\icon\drawable-hdpi-icon.png
+git checkout resources\android\icon\drawable-ldpi-icon.png
+git checkout resources\android\icon\drawable-mdpi-icon.png
+git checkout resources\android\icon\drawable-xhdpi-icon.png
+git checkout resources\android\icon\drawable-xxhdpi-icon.png
+git checkout resources\android\icon\drawable-xxxhdpi-icon.png
 popd
