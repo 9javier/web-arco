c83c81f3b6f14ed1bd63f5c67331ce40a1e8c337
Áxel <axel@galvintec.com>
2019-06-13 02:17:16 +0200

    Fix wrong file references on the AL staging build script.

--- _dev/android/build_al_staging.bat
+++ _dev/android/build_al_staging.bat
@@ -2,23 +2,23 @@ pushd %~dp0\..\..
 call ionic cordova plugin remove com.mirasense.scanditsdk.plugin
 call ionic cordova plugin add scandit\
 copy /Y config.staging.xml config.xml
-copy /Y resources.dev\android\icon\drawable-hdpi-icon.png resources\android\icon\drawable-hdpi-icon.png config.xml
-copy /Y resources.dev\android\icon\drawable-ldpi-icon.png resources\android\icon\drawable-ldpi-icon.png config.xml
-copy /Y resources.dev\android\icon\drawable-mdpi-icon.png resources\android\icon\drawable-mdpi-icon.png config.xml
-copy /Y resources.dev\android\icon\drawable-xhdpi-icon.png resources\android\icon\drawable-xhdpi-icon.png config.xml
-copy /Y resources.dev\android\icon\drawable-xxhdpi-icon.png resources\android\icon\drawable-xxhdpi-icon.png config.xml
-copy /Y resources.dev\android\icon\drawable-xxxhdpi-icon.png resources\android\icon\drawable-xxxhdpi-icon.png config.xml
-copy /Y resources.dev\icon.png resources\icon.png config.xml
+copy /Y resources.dev\android\icon\drawable-hdpi-icon.png resources\android\icon\drawable-hdpi-icon.png
+copy /Y resources.dev\android\icon\drawable-ldpi-icon.png resources\android\icon\drawable-ldpi-icon.png
+copy /Y resources.dev\android\icon\drawable-mdpi-icon.png resources\android\icon\drawable-mdpi-icon.png
+copy /Y resources.dev\android\icon\drawable-xhdpi-icon.png resources\android\icon\drawable-xhdpi-icon.png
+copy /Y resources.dev\android\icon\drawable-xxhdpi-icon.png resources\android\icon\drawable-xxhdpi-icon.png
+copy /Y resources.dev\android\icon\drawable-xxxhdpi-icon.png resources\android\icon\drawable-xxxhdpi-icon.png
+copy /Y resources.dev\icon.png resources\icon.png
 copy /Y libs\services\src\environments\environment.staging.ts libs\services\src\environments\environment.ts
 call ionic cordova build android --release --project al
 copy /Y config.xml config.staging.xml
-copy /Y resources\android\icon\drawable-hdpi-icon.png resources.dev\android\icon\drawable-hdpi-icon.png config.xml
-copy /Y resources\android\icon\drawable-ldpi-icon.png resources.dev\android\icon\drawable-ldpi-icon.png config.xml
-copy /Y resources\android\icon\drawable-mdpi-icon.png resources.dev\android\icon\drawable-mdpi-icon.png config.xml
-copy /Y resources\android\icon\drawable-xhdpi-icon.png resources.dev\android\icon\drawable-xhdpi-icon.png config.xml
-copy /Y resources\android\icon\drawable-xxhdpi-icon.png resources.dev\android\icon\drawable-xxhdpi-icon.png config.xml
-copy /Y resources\android\icon\drawable-xxxhdpi-icon.png resources.dev\android\icon\drawable-xxxhdpi-icon.png config.xml
-copy /Y resources\icon.png resources.dev\icon.png config.xml
+copy /Y resources\android\icon\drawable-hdpi-icon.png resources.dev\android\icon\drawable-hdpi-icon.png
+copy /Y resources\android\icon\drawable-ldpi-icon.png resources.dev\android\icon\drawable-ldpi-icon.png
+copy /Y resources\android\icon\drawable-mdpi-icon.png resources.dev\android\icon\drawable-mdpi-icon.png
+copy /Y resources\android\icon\drawable-xhdpi-icon.png resources.dev\android\icon\drawable-xhdpi-icon.png
+copy /Y resources\android\icon\drawable-xxhdpi-icon.png resources.dev\android\icon\drawable-xxhdpi-icon.png
+copy /Y resources\android\icon\drawable-xxxhdpi-icon.png resources.dev\android\icon\drawable-xxxhdpi-icon.png
+copy /Y resources\icon.png resources.dev\icon.png
 copy /Y libs\services\src\environments\environment.ts libs\services\src\environments\environment.staging.ts
 git checkout config.xml
 git checkout resources\android\icon\drawable-hdpi-icon.png
