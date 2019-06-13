6fbdfcc3ce727963e3facccdfee7d75c14625d80
Áxel <axel@galvintec.com>
2019-06-13 02:46:12 +0200

    Fix API URL after being reverted to a previous value at 1422e282

--- libs/services/src/environments/environment.prod.ts
+++ libs/services/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBase: 'http://192.168.254.109/api',
+  apiBase: 'http://192.168.0.36:8080/api',
   sga:{
     client_id:"krack-client-sga",
     client_secret:"fGx4=yU-j4^jAAjZtV+YTDsm-@R$HAK3"
