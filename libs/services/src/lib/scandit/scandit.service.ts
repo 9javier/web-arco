77978fe5fac00efb86a44e7b28df8fc6f926af78
Áxel <axel@galvintec.com>
2019-06-17 10:58:48 +0200

    Fix scanner debug logs being printed with an unusable structure.

--- libs/services/src/lib/scandit/scandit.service.ts
+++ libs/services/src/lib/scandit/scandit.service.ts
@@ -557,20 +557,20 @@ export class ScanditService {
   positioningLog(type: 1|2|3, index, message, ...params: any[]) {
     params = params || [];
     params.unshift(type, "positioning", index, message);
-    this.scannerLog.call(this, params);
+    this.scannerLog.apply(this, params);
   }
 
   pickingLog(type: 1|2|3, index, message, ...params: any[]) {
     params = params || [];
     params.unshift(type, "picking", index, message);
-    this.scannerLog.call(this, params);
+    this.scannerLog.apply(this, params);
   }
 
   scannerLog(type: 1|2|3, operation, index, message, ...params: any[]) {
     params = params || [];
     params.unshift("positioning log", "\t" + index + "\t" + message);
     var logFunction = type === 3 ? console.error : (type === 2 ? console.warn : console.log);
-    logFunction.call(console, params);
+    logFunction.apply(console, params);
   }
 
 }
