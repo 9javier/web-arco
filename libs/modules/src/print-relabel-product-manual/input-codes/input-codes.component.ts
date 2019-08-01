e96b0252a64253a112a7aaad91d405b4802c8f81
Marcelo <marcelo@galvintec.com>
2019-08-01 10:37:11 +0200

    Fix to make work relabel-products-labels for users without warehouse assigned, used to relabel products in warehouse.

--- libs/modules/src/print-relabel-product-manual/input-codes/input-codes.component.ts
+++ libs/modules/src/print-relabel-product-manual/input-codes/input-codes.component.ts
@@ -93,7 +93,12 @@ export class InputCodesComponent implements OnInit {
   }
 
   private async postRelabelProduct(modelId: number, sizeId: number) {
-    let warehouseId = (await this.authService.getWarehouseCurrentUser()).id;
+    let warehouseUser = await this.authService.getWarehouseCurrentUser();
+    let warehouseId = null;
+    if (warehouseUser) {
+      warehouseId = warehouseUser.id;
+    }
+
     this.productsService
       .postRelabel({
         modelId,
