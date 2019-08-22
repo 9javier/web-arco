99f3b729c913608c3ac7422a3c017c6b075466c2
Luis Álvarez <luis.albeos@gmail.com>
2019-08-22 11:25:56 +0200

    Modify zebra plugin to keep open connection to printer

--- plugin-zebra/www/zbtprinter.js
+++ plugin-zebra/www/zbtprinter.js
@@ -12,3 +12,19 @@ exports.find = function(successCallback, errorCallback) {
     cordova.exec(successCallback, errorCallback, 'ZebraBluetoothPrinter', 'find', []);
 };
 
+exports.printWithConnection = function(mac, str, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, 'ZebraBluetoothPrinter', 'printWithConnection', [mac, str]);
+};
+
+exports.openConnection = function(mac, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, 'ZebraBluetoothPrinter', 'openConnection', [mac]);
+};
+
+exports.closeConnection = function(successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, 'ZebraBluetoothPrinter', 'closeConnection', []);
+};
+
+exports.checkConnection = function(successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, 'ZebraBluetoothPrinter', 'checkConnection', []);
+};
+
