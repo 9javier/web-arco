fc658df66d2dd8759ca17856092ac1807654126d
juan gomez <jddios837@gmail.com>
2019-09-03 21:05:36 -0500

    New section welcome
    when user don't have user-time the page section is showed

---
+++ apps/al/src/app/welcome/welcome.page.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'suite-welcome',
+	templateUrl: './welcome.page.html',
+	styleUrls: ['./welcome.page.scss']
+})
+export class WelcomeComponent implements OnInit {
+	constructor() { }
+
+	ngOnInit(): void { }
+}
