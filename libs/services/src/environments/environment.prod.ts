cab046329522d216af90790593e940f2a11acddb
Marcelo <marcelo@galvintec.com>
2019-09-05 11:03:10 +0200

    Add urlBase to project environment and use it to load products photos in prices list.

--- libs/services/src/environments/environment.prod.ts
+++ libs/services/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   apiBase: 'http://mga.krack.es:8080/api',
+  urlBase: 'http://mga.krack.es:8080',
   sga:{
     client_id:"krack-client-sga",
     client_secret:"fGx4=yU-j4^jAAjZtV+YTDsm-@R$HAK3"
