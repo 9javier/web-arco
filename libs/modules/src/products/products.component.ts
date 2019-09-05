f3c291268937c7f9b95dd86b0ea24f9576a10a50
juan gomez <jddios837@gmail.com>
2019-09-05 16:04:58 -0500

    Refactor Products & Tariffs
    Removed second consult to search endpoint
    
    Commented Enabled Checkbox in Tariffs

--- libs/modules/src/products/products.component.ts
+++ libs/modules/src/products/products.component.ts
@@ -182,7 +182,6 @@ export class ProductsComponent implements OnInit {
 
   ngOnInit() {
     console.log(this.form);
-    //this.initProducts();
     this.getFilters();
     this.listenChanges();
   }
@@ -208,7 +207,6 @@ export class ProductsComponent implements OnInit {
     this.form.statusChanges.subscribe(change=>{
       if (this.pauseListenFormChange) return;
       ///**format the reference */
-      //this.form.controls.productReferencePattern.patchValue(this.buildReference(this.form.value.productReferencePattern),{emitEvent:false});
       /**cant send a request in every keypress of reference, then cancel the previous request */
       clearTimeout(this.requestTimeout)
       /**it the change of the form is in reference launch new timeout with request in it */
@@ -216,6 +214,7 @@ export class ProductsComponent implements OnInit {
         /**Just need check the vality if the change happens in the reference */
         if(this.form.valid)
           this.requestTimeout = setTimeout(()=>{
+            console.log('Search this.form.statusChanges');            
             this.searchInContainer(this.sanitize(this.getFormValueCopy()));
         },1000);
       }else{
@@ -260,12 +259,6 @@ export class ProductsComponent implements OnInit {
       this.searchsInContainer = searchsInContainer.data.results;
       this.initSelectForm();
       this.dataSource = new MatTableDataSource<InventoryModel.SearchInContainer>(this.searchsInContainer);
-      /*this.updateFilterSourceColors(searchsInContainer.data.filters.colors);
-      this.updateFilterSourceContainers(searchsInContainer.data.filters.containers);
-      this.updateFilterSourceModels(searchsInContainer.data.filters.models);
-      this.updateFilterSourceSizes(searchsInContainer.data.filters.sizes);
-      this.updateFilterSourceWarehouses(searchsInContainer.data.filters.warehouses);
-      this.updateFilterSourceOrdertypes(searchsInContainer.data.filters.ordertypes);*/
       let paginator = searchsInContainer.data.pagination;
       this.paginator.length = paginator.totalResults;
       this.paginator.pageIndex = paginator.page - 1;
@@ -302,26 +295,6 @@ export class ProductsComponent implements OnInit {
         if(warehouseMain.length > 0) {
           warehouse = warehouseMain[0];
         }
-        
-//         this.inventoryServices.searchInContainer({warehouses:[warehouse.id],orderby:{type:TypesService.ID_TYPE_ORDER_PRODUCT_DEFAULT.toLocaleString()},pagination: {page: 1, limit: 0}}).subscribe(searchsInContainer=>{
-//           this.updateFilterSourceWarehouses(searchsInContainer.data.filters.warehouses);
-//           this.updateFilterSourceColors(searchsInContainer.data.filters.colors);
-//           this.updateFilterSourceContainers(searchsInContainer.data.filters.containers);
-//           this.updateFilterSourceModels(searchsInContainer.data.filters.models);
-//           this.updateFilterSourceSizes(searchsInContainer.data.filters.sizes);
-//           this.updateFilterSourceOrdertypes(searchsInContainer.data.filters.ordertypes);
-//           setTimeout(() => {
-//             this.pauseListenFormChange = false;
-// -            this.searchInContainer(this.sanitize(this.getFormValueCopy()));
-//             this.pauseListenFormChange = true;
-//             this.form.get("warehouses").patchValue([warehouse.id], {emitEvent: false});
-//             this.form.get("orderby").get("type").patchValue("" + TypesService.ID_TYPE_ORDER_PRODUCT_DEFAULT, {emitEvent: false});
-//             setTimeout(() => {
-//               this.pauseListenFormChange = false;
-//               this.searchInContainer(this.sanitize(this.getFormValueCopy()));
-//             }, 0);
-//           }, 0);
-//         },
 
         this.inventoryServices.searchFilters({}).subscribe(searchsInContainer=>{
           this.updateFilterSourceWarehouses(searchsInContainer.data.filters.warehouses);
@@ -332,7 +305,6 @@ export class ProductsComponent implements OnInit {
           this.updateFilterSourceOrdertypes(searchsInContainer.data.filters.ordertypes);
           setTimeout(() => {
             this.pauseListenFormChange = false;
--            this.searchInContainer(this.sanitize(this.getFormValueCopy()));
             this.pauseListenFormChange = true;
             this.form.get("warehouses").patchValue([warehouse.id], {emitEvent: false});
             this.form.get("orderby").get("type").patchValue("" + TypesService.ID_TYPE_ORDER_PRODUCT_DEFAULT, {emitEvent: false});
