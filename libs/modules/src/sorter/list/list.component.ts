e4128be788f5a55d84ae78074dae3904a46333fb
Marco Useche <usechermarcoj@gmail.com>
2019-09-19 10:00:50 -0400

    rails table modified at edit template modal add

--- libs/modules/src/sorter/list/list.component.ts
+++ libs/modules/src/sorter/list/list.component.ts
@@ -49,13 +49,40 @@ import { RailsConfigurationComponent } from './modals-zone/rails-configuration/r
 })
 export class ListComponent implements OnInit {
 
-  displayedColumns = ['icon', 'delete', 'Ntemplate', 'nombre', 'carriles', 'active', 'configurarCarriles', 'warehoures'];
+  displayedColumns = ['icon', 'updateCarriles', 'delete', 'Ntemplate', 'nombre', 'carriles', 'active', 'configurarCarriles', 'warehoures'];
   dataSource = new ExampleDataSource();
   warehouses: WarehouseModel.Warehouse[] = [];
   displayedColumnsWareHouse: any = ['check', 'name'];
   selectedForm: FormGroup;
   items: FormArray;
   showRails: boolean = false;
+
+  rails = [
+    {
+      height:1,
+      columns:[
+        {column:1, ways_number:1},
+        {column:2, ways_number:2},
+        {column:3, ways_number:3}
+      ]
+    },
+    {
+      height:2,
+      columns:[
+        {column:1, ways_number:4},
+        {column:2, ways_number:5},
+        {column:3, ways_number:6}
+      ]
+    },
+    {
+      height:3,
+      columns:[
+        {column:1, ways_number:7},
+        {column:2, ways_number:8},
+        {column:3, ways_number:9}
+      ]
+    }
+  ]
 
   constructor(
     private crudService: CrudService,
@@ -139,6 +166,45 @@ export class ListComponent implements OnInit {
     modal.present();
   }
 
+  getColumn(index: number, column: number, height: number): void {
+    //get right and left values
+    this.rails.forEach(rail => {
+      if(rail.height == height) {
+       let columns = rail.columns;
+       for(let i = 0; i < columns.length; i++) 
+        if(column == columns[i].ways_number) {
+          console.log('Current: '+column)
+          if(columns[i-1]) {
+            console.log('Left: '+columns[i-1].ways_number)
+          }
+          if(columns[i+1]) {
+            console.log('Right: '+columns[i+1].ways_number)
+          }
+        }
+      }
+    })
+
+    //get top and bottom
+    for(let i = 0; i < this.rails.length; i++) {
+      if(this.rails[i].height == height) {
+          if(this.rails[i-1]) {
+            for(let j = 0; j < this.rails[i-1].columns.length; j++) {
+              if(j == index){
+                console.log('Top: '+ this.rails[i-1].columns[j].ways_number)
+              }
+            }
+          }
+          if(this.rails[i+1]) {
+            for(let j = 0; j < this.rails[i+1].columns.length; j++) {
+              if(j == index){
+                console.log('Bottom: '+ this.rails[i+1].columns[j].ways_number)
+              }
+            }
+          }
+      }
+    }
+  }
+
   activeDelete() {
     event.stopPropagation();
   }
@@ -167,6 +233,7 @@ export class ListComponent implements OnInit {
   }
 
   displayRails() {
+    event.stopPropagation();
     this.showRails = !this.showRails;
   }
 }
