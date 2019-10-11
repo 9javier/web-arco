9a84987623ed91aacf5582adec28ee416d7ddb04
Marcelo <marcelo@galvintec.com>
2019-10-11 12:31:51 +0200

    Create a custom local-storage provider to use local-storage native from JS. Replace use of ionic storage by custom provider.

---
+++ libs/services/src/providers/local-storage/local-storage.provider.ts
@@ -0,0 +1,48 @@
+import {Injectable} from "@angular/core";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalStorageProvider {
+
+  public KEYS = {
+    USERNAME: 'username',
+    ACCESS_TOKEN: 'access_token',
+    USER_ID: 'user_id',
+    USER: 'user',
+    REFRESH_TOKEN: 'refreshToken',
+    DICTIONARY_ACCESS: 'dictionaryAcess'
+  };
+
+  public set(key: string, value: any) {
+    return new Promise((resolve, reject) => {
+      localStorage.setItem(key, value);
+      resolve();
+    });
+  }
+
+  public get(key: string) {
+    return new Promise((resolve, reject) => {
+      let value = localStorage.getItem(key);
+      if (typeof value != 'undefined' && value != null) {
+        resolve(value);
+      } else {
+        reject(`Error to try get ${key} from LocalStorage`);
+      }
+    });
+  }
+
+  public remove(key: string) {
+    return new Promise((resolve, reject) => {
+      localStorage.removeItem(key);
+      resolve();
+    });
+  }
+
+  public clear() {
+    return new Promise((resolve, reject) => {
+      localStorage.clear();
+      resolve();
+    });
+  }
+}
