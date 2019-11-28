68cbc177f1c234033782bce2fc5a6f67aa4ba2c2
Marcelo <marcelo@galvintec.com>
2019-11-28 14:45:21 +0100

    Exclude from interceptor relabel products page to redirect the user to login because it manage the request errors itself.

--- libs/services/src/lib/interceptors/addTokenToRequestInterceptor.ts
+++ libs/services/src/lib/interceptors/addTokenToRequestInterceptor.ts
@@ -1,31 +1,23 @@
-import { Injectable } from '@angular/core';
-
-import { AuthenticationService } from '../../../../services/src/lib/endpoint/authentication/authentication.service';
-
-
-import { Router, ActivatedRoute, Route } from '@angular/router';
-import { empty } from 'rxjs/internal/Observer';
-
-import {
-  HttpInterceptor,
-  HttpRequest,
-  HttpResponse,
-  HttpHandler,
-  HttpEvent,
-  HttpErrorResponse
-} from '@angular/common/http';
-import { Observable, throwError, from, BehaviorSubject } from 'rxjs';
-import { catchError, map, switchMap, finalize, filter, take } from 'rxjs/operators';
-import { Oauth2Service } from '../endpoint/oauth2/oauth2.service';
-import { IntermediaryService } from '../endpoint/intermediary/intermediary.service';
-import { ToastController } from "@ionic/angular";
+import {Injectable} from '@angular/core';
+import {AuthenticationService} from '../../../../services/src/lib/endpoint/authentication/authentication.service';
+import {Router, ActivatedRoute} from '@angular/router';
+import {empty} from 'rxjs/internal/Observer';
+import {HttpInterceptor, HttpRequest, HttpHandler, HttpEvent} from '@angular/common/http';
+import {Observable, from, BehaviorSubject} from 'rxjs';
+import {catchError, map, switchMap, finalize, filter, take} from 'rxjs/operators';
+import {Oauth2Service} from '../endpoint/oauth2/oauth2.service';
+import {IntermediaryService} from '../endpoint/intermediary/intermediary.service';
+import {ToastController} from "@ionic/angular";
+import {Location} from "@angular/common";
 
 @Injectable()
 export class AddTokenToRequestInterceptor implements HttpInterceptor {
+
   constructor(
     private authenticationService: AuthenticationService,
     private intermediaryService: IntermediaryService,
     private router: Router,
+    private location: Location,
     private route: ActivatedRoute,
     private oauth2Service: Oauth2Service,
     private toastController: ToastController
@@ -35,25 +27,30 @@ export class AddTokenToRequestInterceptor implements HttpInterceptor {
   tokenSubject: BehaviorSubject<string> = new BehaviorSubject<string>(null);
   private isToastVisible: boolean = false;
 
+  private listRoutesWithCustomErrorConnectionManagement: string[] = [
+    '/print/product/relabel'
+  ];
+
   addTokenToRequest(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return from(this.authenticationService.getCurrentToken()).pipe(switchMap(token => {
       if (!token && !(request.url.includes("/api/oauth2/"))) {
-        this.authenticationService.logout()
+        this.authenticationService.logout();
         return Observable.create(empty);
       } else if (token) {
         return next.handle(!(request.url.includes("/api/oauth2/")) ? request.clone({
           setHeaders: {
-            Authorization: `${token}`
+            Authorization: token
           }
-        }) : request).
+        }) : request)
           /**Catch http response error to detect if it is 401 or 403(authentication) */
-          pipe(catchError((err, caught) => {
-            switch (err.status) {
+          .pipe(catchError((err, caught) => {
+          switch (err.status) {
               case 403:
                 return this.handle401Error(request, next);
               case 401:
-                if (!request.url.includes("/api/oauth2/"))
+                if (!request.url.includes("/api/oauth2/")) {
                   return this.handle401Error(request, next);
+                }
                 this.authenticationService.logout();
                 return new Observable(observer => observer.error(err));
               case 400:
@@ -75,7 +72,14 @@ export class AddTokenToRequestInterceptor implements HttpInterceptor {
                 }
                 break;
               case 0:
-                if (err.statusText == 'Unknown Error' && this.authenticationService.isAuthenticated()) {
+                // check if the page is assigned to exclude that will manage itself the connection error
+                let customErrorManagement = !!this.listRoutesWithCustomErrorConnectionManagement.find(url => url === this.router.url);
+                /*
+  * NO CONNECTION TO API
+  * status = 0
+  * statusText = 'Unknown Error'
+  * */
+                if (!customErrorManagement && err.statusText == 'Unknown Error' && this.authenticationService.isAuthenticated()) {
                   this.authenticationService.logout();
                   if (!this.isToastVisible) {
                     this.presentToast('Ha ocurrido un error al conectar con el servidor.', 'danger');
@@ -91,8 +95,6 @@ export class AddTokenToRequestInterceptor implements HttpInterceptor {
       }
     }));
   }
-
-
 
   /**
    * Handle the http 401 error(authentication) to request a new refresh token
@@ -143,11 +145,7 @@ export class AddTokenToRequestInterceptor implements HttpInterceptor {
     }
   }
 
-
-  intercept(
-    request: HttpRequest<any>,
-    next: HttpHandler
-  ): Observable<HttpEvent<any>> {
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return this.addTokenToRequest(request, next);
   }
 
