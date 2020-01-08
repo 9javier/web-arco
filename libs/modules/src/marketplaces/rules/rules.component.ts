33b9d166cbf15aa52736a880a23a6bce7ce2d912
Marco Useche <usechermarcoj@gmail.com>
2020-01-08 12:51:06 -0400

    feature: add rules tables tabs

--- libs/modules/src/marketplaces/rules/rules.component.ts
+++ libs/modules/src/marketplaces/rules/rules.component.ts
@@ -8,6 +8,16 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class RulesComponent implements OnInit {
 
+  categories_data = [
+    {
+      name: 'Mujer', 
+      categories: 'Mujer, mujer outlet, mujer rebajas, todo mujer', 
+      product_quantity: '3655 productos'}
+  ];
+
+  displayedCategoriesColumns: string[] = ['name', 'categories', 'product_quantity'];
+  dataSourceCategories = this.categories_data;
+
   constructor(private route: ActivatedRoute) { 
     console.log(this.route.snapshot.data['name'])
   }
