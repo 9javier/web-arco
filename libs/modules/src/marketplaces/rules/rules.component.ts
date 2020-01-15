41488f5bd35be5aa344a79cc883f87217849d2ae
Daniel Otero <daniel@galvintec.com>
2020-01-15 14:58:39 +0100

    Add new rules page structure

--- libs/modules/src/marketplaces/rules/rules.component.ts
+++ libs/modules/src/marketplaces/rules/rules.component.ts
@@ -10,15 +10,134 @@ import { NewRuleComponent } from './new-rule/new-rule.component';
 })
 export class RulesComponent implements OnInit {
 
-  categories_data = [
+  private dataSourceCategories = [
     {
+      id: 1,
       name: 'Mujer', 
-      categories: 'Mujer, mujer outlet, mujer rebajas, todo mujer', 
-      product_quantity: '3655 productos'}
+      categories: [
+        {
+          id: 1,
+          group: 2,
+          name: 'Mujer'
+        },
+        {
+          id: 2,
+          group: 2,
+          name: 'mujer outlet'
+        },
+        {
+          id: 3,
+          group: 2,
+          name: 'mujer rebajas'
+        },
+        {
+          id: 4,
+          group: 2,
+          name: 'todo mujer'
+        }
+      ],
+      products: 3655
+    },
+    {
+      id: 2,
+      name: 'Hombre',
+      categories: [
+        {
+          id: 5,
+          group: 2,
+          name: 'Hombre'
+        },
+        {
+          id: 6,
+          group: 2,
+          name: 'hombre outlet'
+        },
+        {
+          id: 7,
+          group: 2,
+          name: 'hombre rebajas'
+        },
+        {
+          id: 8,
+          group: 2,
+          name: 'todo hombre'
+        }
+      ],
+      products: 3655
+    },
+    {
+      id: 3,
+      name: 'Kids',
+      categories: [
+        {
+          id: 9,
+          group: 2,
+          name: 'Niño'
+        },
+        {
+          id: 10,
+          group: 2,
+          name: 'niña'
+        },
+        {
+          id: 11,
+          group: 2,
+          name: 'kids rebajas'
+        },
+        {
+          id: 12,
+          group: 2,
+          name: 'kids outlet'
+        }
+      ],
+      products: 3655
+    }
   ];
 
-  displayedCategoriesColumns: string[] = ['name', 'categories', 'product_quantity', 'edit'];
-  dataSourceCategories = this.categories_data;
+  private displayedCategoriesColumns: string[] = ['name', 'categories', 'products', 'edit'];
+
+  private dataSourcePrice = [
+    {
+      id: 1,
+      name: 'Mayores de 100 €',
+      price: 100,
+      products: 343
+    },
+    {
+      id: 2,
+      name: 'Mayores de 300 €',
+      price: 300,
+      products: 343
+    },
+    {
+      id: 3,
+      name:  'Mayores de 433.99 €',
+      price: 433.99,
+      products: 343
+    }
+  ];
+
+  private displayedPriceColumns: string[] = ['name', 'price', 'products', 'edit'];
+
+  private dataSourceStocks = [
+    {
+      name: 'Stock mayor que 30',
+      stock: 30,
+      products: 7894
+    },
+    {
+      name: 'Stock mayor que 58',
+      stock: 58,
+      products: 2344
+    },
+    {
+      name: 'Stock mayor que 25',
+      stock: 25,
+      products: 9665
+    }
+  ];
+
+  private displayedStocksColumns: string[] = ['name', 'stock', 'products', 'edit'];
 
   constructor(
     private route: ActivatedRoute,
@@ -28,14 +147,30 @@ export class RulesComponent implements OnInit {
   }
 
   ngOnInit() {
+    for (let ruleByPrice of this.dataSourcePrice) {
+      ruleByPrice.price = <any>ruleByPrice.price.toFixed(2);
+    }
   }
 
-  async openModalNewRule(): Promise<void> {
-    let modal = (await this.modalController.create({
-      component: NewRuleComponent
-    }));
-    modal.onDidDismiss().then((response) => {
-     console.log(response)
+  async openModalNewRule(ruleFilterType): Promise<void> {
+    let modal = await this.modalController.create({
+      component: NewRuleComponent,
+      componentProps: {
+        ruleFilterType,
+        mode: 'create'
+      }
+    });
+    modal.present();
+  }
+
+  async editRule(ruleFilterType, rule): Promise<void> {
+    let modal = await this.modalController.create({
+      component: NewRuleComponent,
+      componentProps: {
+        ruleFilterType,
+        rule,
+        mode: 'edit'
+      }
     });
     modal.present();
   }
