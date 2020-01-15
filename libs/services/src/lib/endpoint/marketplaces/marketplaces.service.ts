6e54bb1da89023ca3970c018b9653f87ebf000fe
Marco Useche <usechermarcoj@gmail.com>
2020-01-15 09:04:14 -0400

    feature: add new services methods to marketplaces services

--- libs/services/src/lib/endpoint/marketplaces/marketplaces.service.ts
+++ libs/services/src/lib/endpoint/marketplaces/marketplaces.service.ts
@@ -13,21 +13,55 @@ export class MarketplacesService {
 
   private apiBase = 'https://localhost:5001/api';
   private getMapDataRulesUrl = this.apiBase + "/MapDataRules";
+  private postMapDataRulesUrl = this.apiBase + "/MapDataRules";
   private getMapEntitiesUrl = this.apiBase + "/EnumMetadata/get/mapentity";
+  private getRulesFilterTypesUrl = this.apiBase + "/EnumMetadata/get/rulefiltertype";
+  private getRulesFilterUrl = this.apiBase + "/RuleFilter";
+  private postRulesFilterUrl = this.apiBase + "/RuleFilter";
+  private getMarketsUrl = this.apiBase + "/Markets";
 
   constructor(
-    private http: HttpClient,
-    private auth: AuthenticationService,
-    private requestsProvider: RequestsProvider
+    private http: HttpClient
   ) {}
 
-  getMapEntities():Observable<any>{ 
+  getMapEntities():Observable<any> { 
     return this.http.get<any>(this.getMapEntitiesUrl, {}).pipe(map(response=>{
       return response;
     }));
   }
-  getMapDataRules():Observable<any>{ 
+
+  getMapDataRules():Observable<any> { 
     return this.http.get<any>(this.getMapDataRulesUrl, {}).pipe(map(response=>{
+      return response;
+    }));
+  }
+
+  postMapDataRules(data):Observable<any> { 
+    return this.http.post<any>(this.postMapDataRulesUrl, data, {}).pipe(map(response=>{
+      return response;
+    }));
+  }
+
+  getRulesFilterTypes():Observable<any> {
+    return this.http.get<any>(this.getRulesFilterTypesUrl, {}).pipe(map(response=>{
+      return response;
+    }));
+  }
+
+  getRulesFilter():Observable<any> {
+    return this.http.get<any>(this.getRulesFilterUrl, {}).pipe(map(response=>{
+      return response;
+    }));
+  }
+
+  postRulesFilter(data):Observable<any> {
+    return this.http.post<any>(this.getRulesFilterUrl, data, {}).pipe(map(response=>{
+      return response;
+    }));
+  }
+
+  getMarkets():Observable<any> {
+    return this.http.get<any>(this.getMarketsUrl, {}).pipe(map(response=>{
       return response;
     }));
   }
