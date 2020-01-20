6858d19d694538c7cc742ef4e21c4cdcf78f7ace
jpmejia <cromeocromeo@gmail.com>
2020-01-20 14:04:37 -0400

    predistribution save status blocked

--- libs/services/src/lib/endpoint/predistributions/predistributions.service.ts
+++ libs/services/src/lib/endpoint/predistributions/predistributions.service.ts
@@ -22,10 +22,12 @@ export class PredistributionsService {
   private baseUrl: string;
   private indexUrl: string;
   private entitiesUrl: string;
+  private updateBlockReservedUrl: string;
   constructor(private http: HttpClient) {
-    this.baseUrl = environment.apiSorter
-    this.indexUrl = `${this.baseUrl}/reception/expedition/lines-destiny-impress`
-    this.entitiesUrl = `${this.baseUrl}/reception/expedition/lines-destiny-impress/entites`
+    this.baseUrl = environment.apiSorter;
+    this.indexUrl = `${this.baseUrl}/reception/expedition/lines-destiny-impress`;
+    this.entitiesUrl = `${this.baseUrl}/reception/expedition/lines-destiny-impress/entites`;
+    this.updateBlockReservedUrl = `${this.baseUrl}/reception/expedition/update-block-reserved`;
   }
 
   index(body: PredistributionModel.IndexRequest): Observable<PredistributionModel.DataSource> {
@@ -47,89 +49,10 @@ export class PredistributionsService {
       map(resp => resp.data)
     )
   }
-  // async getIndex(): Promise<Observable<HttpResponse<PredistributionModel.ResponseIndex>>> {
-  //   const currentToken = await this.auth.getCurrentToken();
-  //   const headers = new HttpHeaders({ Authorization: currentToken });
-  //   return this.http.get<PredistributionModel.ResponseIndex>(this.getIndexUrl, {
-  //     headers: headers,
-  //     observe: 'response'
-  //   });
-  // }
+  updateBlockReserved(data:PredistributionModel.BlockReservedRequest[]){
+    return this.http.post<HttpRequestModel.Response>(this.updateBlockReservedUrl, data).pipe(
+      map(resp => resp.data)
+    )
+  }
 
-  // async getIndex() {
-  //   const warehouse: Warehouse = {
-  //     id: 1,
-  //     name: 'Tienda 1'
-  //   };
-
-  //   const model: Model = {
-  //     id: 1,
-  //     reference: ''
-  //   };
-
-  //   const size: Size = {
-  //     id: 1,
-  //     reference: '',
-  //     number: 1,
-  //     name: ''
-  //   };
-
-  //   const brand: Brand = {
-  //     createdAt: '',
-  //     updatedAt: '',
-  //     id: 1,
-  //     avelonId: 1,
-  //     datasetHash: '',
-  //     name: '',
-  //     supplierName: ''
-  //   };
-
-  //   const season: Season = {
-  //     createdAt: '',
-  //     updatedAt: '',
-  //     id: 1,
-  //     datasetHash: '',
-  //     name: '',
-  //     avelonId: '',
-  //   };
-
-  //   const article: Product = {
-  //       id: 1,
-  //       reference: 'Articulo 1',
-  //       initialWarehouse: warehouse,
-  //       model: model,
-  //       size: size,
-  //       brand: brand,
-  //       season: season,
-
-  //   };
-
-  //   const date_service = '12, Dec, 2019';
-
-  //   const mock: ({ reserved: boolean; id: number; date_service: string; warehouse: Warehouse; distribution: boolean; article: Product })[] = [
-  //     { id: 1, warehouse, article, date_service, distribution: false, reserved: false },
-  //     { id: 2, warehouse, article, date_service, distribution: false, reserved: false },
-  //     { id: 3, warehouse, article, date_service, distribution: true, reserved: false },
-  //     { id: 4, warehouse, article, date_service, distribution: true, reserved: false },
-  //     { id: 5, warehouse, article, date_service, distribution: true, reserved: false },
-  //     { id: 6, warehouse, article, date_service, distribution: false, reserved: true },
-  //     { id: 7, warehouse, article, date_service, distribution: false, reserved: false },
-  //     { id: 8, warehouse, article, date_service, distribution: false, reserved: false },
-  //     { id: 9, warehouse, article, date_service, distribution: true, reserved: false },
-  //     { id: 10, warehouse, article, date_service, distribution: false, reserved: false },
-  //     { id: 11, warehouse, article, date_service, distribution: false, reserved: false },
-  //     { id: 12, warehouse, article, date_service, distribution: false, reserved: true },
-  //     { id: 13, warehouse, article, date_service, distribution: false, reserved: false },
-  //     { id: 14, warehouse, article, date_service, distribution: false, reserved: false },
-  //     { id: 15, warehouse, article, date_service, distribution: false, reserved: false },
-  //     { id: 16, warehouse, article, date_service, distribution: false, reserved: false },
-  //     { id: 17, warehouse, article, date_service, distribution: false, reserved: false },
-  //     { id: 18, warehouse, article, date_service, distribution: false, reserved: false },
-  //     { id: 19, warehouse, article, date_service, distribution: false, reserved: false },
-  //     { id: 20, warehouse, article, date_service, distribution: false, reserved: false },
-  //     { id: 21, warehouse, article, date_service, distribution: false, reserved: false },
-  //   ];
-
-  //   return mock;
-  // }
 }
