12da38d8e2ad96a84a9f30372323ac4a201047ca
Jasiel Izaguirre <jasiel_izaguirre@hotmail.com>
2020-01-22 23:13:15 -0500

    - Added content page - Added Style - Added logical and Service

--- libs/modules/src/role-assignment/role-assignment.module.ts
+++ libs/modules/src/role-assignment/role-assignment.module.ts
@@ -2,6 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { RoleAssignmentComponent } from './role-assignment.component';
+import { IonicModule } from '@ionic/angular';
+import { BreadcrumbModule, ComponentsModule } from '@suite/common-modules';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ResponsiveLayoutModule } from '../components/responsive-layout/responsive-layout.module';
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatExpansionModule, MatFormFieldModule, MatIconModule,
+  MatListModule, MatSelectModule,
+  MatSlideToggleModule,
+  MatTableModule, MatTooltipModule
+} from '@angular/material';
 
 const routes: Routes = [
   {
@@ -15,6 +27,17 @@ const routes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
+    IonicModule,
+    ResponsiveLayoutModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    FormsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule
   ]
 })
 export class RoleAssignmentModule { }
