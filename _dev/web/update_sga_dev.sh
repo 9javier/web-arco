4d74ca68bdf8502b6feded54e90e442b12007e7d
Áxel <axel@galvintec.com>
2020-01-24 16:15:24 +0100

    Change the dev server update procedure because when running pull as su stopped working.

--- _dev/web/update_sga_dev.sh
+++ _dev/web/update_sga_dev.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
-su
-su sga
+#su
+#su sga
+eval $(ssh-agent -s)
+ssh-add /home/sga/.ssh/id_rsa
 cd /home/sga/sga-app-logistica-krack/ && git reset --hard && git pull && ionic build --project sga --configuration=dev --prod -- --base-href /sga/ && rm -rf ../content && cp -R dist/apps ../content && cp ../content/sga/index.html ../content/index.html
