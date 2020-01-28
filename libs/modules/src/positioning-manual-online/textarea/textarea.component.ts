a477297b4f2e92c7c641a21468d0791338025b3d
jpmejia <cromeocromeo@gmail.com>
2020-01-28 17:33:04 -0300

    k20-1831 brand product no available pocisioning manual

--- libs/modules/src/positioning-manual-online/textarea/textarea.component.ts
+++ libs/modules/src/positioning-manual-online/textarea/textarea.component.ts
@@ -208,7 +208,7 @@ export class TextareaComponent implements OnInit {
     params.noOnline = true;
     this.intermediaryService.presentLoading();
     this.inventoryService
-      .postStore(params)
+      .postStoreOnline(params)
       .then(async (res: InventoryModel.ResponseStore) => {
         this.intermediaryService.dismissLoading();
         if (res.code === 200 || res.code === 201) {
