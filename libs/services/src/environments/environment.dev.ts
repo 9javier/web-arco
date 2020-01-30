170047bb5b17ef1a9af06b7964f4c1a4353086fa
Daniel Otero <daniel@galvintec.com>
2020-01-30 08:40:11 +0100

    Add new apis urls

--- libs/services/src/environments/environment.dev.ts
+++ libs/services/src/environments/environment.dev.ts
@@ -3,6 +3,7 @@ export const environment = {
   apiBase: 'http://192.168.254.111:8080/api',
   apiSorter: 'http://192.168.254.111:8080/api',
   urlBase: 'http://192.168.254.111:8080',
+  apiRule: "http://192.168.254.111:5000/api",
   sga:{
     client_id:"krack-client-sga",
     client_secret:"fGx4=yU-j4^jAAjZtV+YTDsm-@R$HAK3"
