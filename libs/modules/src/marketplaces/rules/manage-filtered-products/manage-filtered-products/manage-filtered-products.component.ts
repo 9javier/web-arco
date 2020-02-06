827010700d414fc590a1e7ff1f9ae0ec68edeecd
Marco Useche <usechermarcoj@gmail.com>
2020-02-06 15:46:13 -0400

    feature: list references products modal modified

--- libs/modules/src/marketplaces/rules/manage-filtered-products/manage-filtered-products/manage-filtered-products.component.ts
+++ libs/modules/src/marketplaces/rules/manage-filtered-products/manage-filtered-products/manage-filtered-products.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import {ModalController} from '@ionic/angular';
-import { MatTableDataSource } from '@angular/material';
+import { MatTableDataSource, MatPaginator } from '@angular/material';
 
 @Component({
   selector: 'suite-manage-filtered-products',
@@ -19,17 +19,43 @@ export class ManageFilteredProductsComponent implements OnInit {
       name: 'product2',
       reference: 'reference2',
       type: 'Excluyente'
+    },
+    { 
+      name: 'product1',
+      reference: 'reference1',
+      type: 'Incluyente'
+    },
+    { 
+      name: 'product2',
+      reference: 'reference2',
+      type: 'Excluyente'
+    },
+    { 
+      name: 'product1',
+      reference: 'reference1',
+      type: 'Incluyente'
+    },
+    { 
+      name: 'product2',
+      reference: 'reference2',
+      type: 'Excluyente'
     }
   ];
 
-  displayedColumns: string[] = ['name', 'reference', 'type', 'delete'];
-  dataSource  = new MatTableDataSource(this.productReferences);
+  @ViewChild('paginatorReferencesNotFilter') paginatorReferencesNotFilter: MatPaginator;
+  @ViewChild('paginatorReferencesFilter') paginatorReferencesFilter: MatPaginator;
+
+  displayedColumns: string[] = ['name', 'reference'];
+  dataSourceReferencesFilter  = new MatTableDataSource(this.productReferences);
+  dataSourceReferencesNotFilter  = new MatTableDataSource(this.productReferences);
 
   constructor(
     private modalController: ModalController
   ) { }
 
   ngOnInit() {
+    setTimeout(() => this.dataSourceReferencesFilter.paginator = this.paginatorReferencesFilter);
+    setTimeout(() => this.dataSourceReferencesNotFilter.paginator = this.paginatorReferencesNotFilter);
   }
 
   close(data) {
