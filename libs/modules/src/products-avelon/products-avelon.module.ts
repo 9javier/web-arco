5c8d8834c9cf20bd72ed87241be4f0c81f0ea5b2
Saul Moncivais <saul.armando667@gmail.com>
2020-02-10 20:46:05 -0600

    In this commit was added the endpoint of notify to avelon

--- libs/modules/src/products-avelon/products-avelon.module.ts
+++ libs/modules/src/products-avelon/products-avelon.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { MatTableModule } from '@angular/material';
 import { MatListModule } from '@angular/material';
 import { ProductsAvelonComponent } from './products-avelon.component';
 import { RouterModule, Routes } from "@angular/router";
-import { MatPaginatorModule } from '@angular/material';
 import { BreadcrumbModule } from '../components/breadcrumb/breadcrumb.module';
 import { TagsInputModule } from '../components/tags-input/tags-input.module';
 import { PaginatorComponentModule } from '../components/paginator/paginator.component.module';
 import { FilterButtonModule } from "../components/filter-button/filter-button.module";
 import { MatTooltipModule } from "@angular/material";
-
+import {
+  MatCheckboxModule,
+  MatPaginatorModule,
+  MatRippleModule,
+  MatSortModule,
+  MatTableModule
+} from '@angular/material';
 
 const routes: Routes = [
   {
@@ -34,7 +38,11 @@ const routes: Routes = [
     TagsInputModule,
     PaginatorComponentModule,
     FilterButtonModule,
-    MatTooltipModule
+    MatTooltipModule,
+    FormsModule,
+    MatCheckboxModule,
+    MatRippleModule,
+    MatSortModule
   ],
   declarations: [ProductsAvelonComponent]
 })
