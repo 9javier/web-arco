e7ceddaa67ba96b8c5a6eff92d61ad9bb280e485
Marcelo <marcelo@galvintec.com>
2020-02-20 18:30:22 +0100

    Fix selection of data to search products in expedition reception to search correct value in models grouped by id.

--- libs/modules/src/receptions-avelon-app/components/manual-reception/manual-reception.component.ts
+++ libs/modules/src/receptions-avelon-app/components/manual-reception/manual-reception.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {ModalController} from "@ionic/angular";
 import {FilterItemsListComponent} from "../filter-items-list/filter-items-list.component";
-import {ReceptionsAvelonService} from "@suite/services";
+import {ReceptionAvelonModel, ReceptionsAvelonService} from "@suite/services";
 import {ReceptionAvelonProvider} from "../../../../../services/src/providers/reception-avelon/reception-avelon.provider";
 import {LoadingMessageComponent} from "../../../components/loading-message/loading-message.component";
 import {ToolbarProvider} from "../../../../../services/src/providers/toolbar/toolbar.provider";
@@ -16,10 +16,11 @@ export class ManualReceptionComponent implements OnInit, OnDestroy {
 
   @ViewChild(LoadingMessageComponent) loadingMessageComponent: LoadingMessageComponent;
 
-  public brandSelected: {id: number, name: string} = null;
-  public modelSelected: {id: number, name: string} = null;
-  public colorSelected: {id: number, name: string} = null;
-  public sizeSelected: {id: number, name: string} = null;
+  public brandSelected: ReceptionAvelonModel.Data = null;
+  public modelSelected: ReceptionAvelonModel.Data = null;
+  public modelIdSelected: number = null;
+  public colorSelected: ReceptionAvelonModel.Data = null;
+  public sizeSelected: ReceptionAvelonModel.Data = null;
 
   private listBrands: any[] = [];
   private listModels: any[] = [];
@@ -54,6 +55,7 @@ export class ManualReceptionComponent implements OnInit, OnDestroy {
   private resetData() {
     this.brandSelected = null;
     this.modelSelected = null;
+    this.modelIdSelected = null;
     this.colorSelected = null;
     this.sizeSelected = null;
     this.listBrands = [];
@@ -118,7 +120,7 @@ export class ManualReceptionComponent implements OnInit, OnDestroy {
         } else if (data.data.filterListType == 'Tallas') {
           this.sizeSelected = data.data.itemSelected;
         }
-        this.updateFilterLists(data.data.itemSelected);
+        this.updateFilterLists(data.data.itemSelected, data.data.filterListType);
       }
     });
 
@@ -134,7 +136,7 @@ export class ManualReceptionComponent implements OnInit, OnDestroy {
       brandId: this.brandSelected.id,
       colorId: this.colorSelected.id,
       sizeId: this.sizeSelected.id,
-      modelId: this.modelSelected.id
+      modelId: this.modelIdSelected
     };
     this.receptionsAvelonService
       .printReceptionLabel(params)
@@ -168,7 +170,7 @@ export class ManualReceptionComponent implements OnInit, OnDestroy {
       });
   }
 
-  private updateFilterLists(filterUsed) {
+  private updateFilterLists(filterUsed, typeFilter: string) {
     let listModels = [];
     let listBrands = [];
     let listSizes = [];
@@ -176,62 +178,47 @@ export class ManualReceptionComponent implements OnInit, OnDestroy {
 
     if (filterUsed.belongsModels) {
       filterUsed.belongsModels.forEach(modelId => {
-        const modelsFilter = this.listModels.filter(model => model.id === modelId);
+        const modelsFilter = this.listModels.filter(model => !!model.available_ids.find(id => id == modelId));
         modelsFilter.forEach(m => {
           const modelFind = listModels.find(elem => elem.id == m.id);
-          if(modelFind === undefined) {
-            if (m.state == 0) {
-              listModels.push(m)
-            } else {
-              listModels.push(m)
-            }
-
+          if (modelFind === undefined) {
+            listModels.push(m)
           }
         });
+
         /***************************brands******************************/
         const brandsFilter = this.listBrands.filter(elem => {
-          if(elem.belongsModels.find(elem => elem === modelId)){
+          if (elem.belongsModels.find(elem => elem === modelId)) {
             return elem
           }
         });
         brandsFilter.forEach(elem => {
-          if(listBrands.find(data => data.id === elem.id) === undefined) {
-            if (elem.state == 0) {
-              listBrands.push(elem)
-            } else {
-              listBrands.push(elem)
-            }
+          if (listBrands.find(data => data.id === elem.id) === undefined) {
+            listBrands.push(elem)
           }
         });
 
         /****************************sizes*****************************/
         const sizesFilter = this.listSizes.filter(elem => {
-          if(elem.belongsModels.find(elem => elem === modelId)){
+          if (elem.belongsModels.find(elem => elem === modelId)) {
             return elem
           }
         });
         sizesFilter.forEach(elem => {
-          if(listSizes.find(data => data.id === elem.id) === undefined) {
-            if (elem.state == 0) {
-              listSizes.push(elem)
-            } else {
-              listSizes.push(elem)
-            }
+          if (listSizes.find(data => data.id === elem.id) === undefined) {
+            listSizes.push(elem)
           }
         });
+
         /*****************************color****************************/
         const colorFilter = this.listColors.filter(elem => {
-          if(elem.belongsModels.find(elem => elem === modelId)){
+          if (elem.belongsModels.find(elem => elem === modelId)) {
             return elem
           }
         });
         colorFilter.forEach(elem => {
-          if(listColors.find(data => data.id === elem.id) === undefined) {
-            if (elem.state == 0) {
-              listColors.push(elem)
-            } else {
-              listColors.push(elem)
-            }
+          if (listColors.find(data => data.id === elem.id) === undefined) {
+            listColors.push(elem)
           }
         })
       })
@@ -239,58 +226,32 @@ export class ManualReceptionComponent implements OnInit, OnDestroy {
       const modelsFilter = this.listModels.filter(model => model.id === filterUsed.id);
       modelsFilter.forEach(m => {
         const modelFind = listModels.find(elem => elem.id == m.id);
-        if(modelFind === undefined) {
-          if (m.state == 0) {
-            listModels.push(m)
-          } else {
-            listModels.push(m)
-          }
+        if (modelFind === undefined) {
+          listModels.push(m)
         }
       });
+
       /***************************brands******************************/
-      const brandsFilter = this.listBrands.filter(elem => {
-        if(elem.belongsModels.find(elem => elem === filterUsed.id)){
-          return elem
-        }
-      });
+      const brandsFilter = this.listBrands.filter(elem => !!elem.belongsModels.find(model => !!filterUsed.available_ids.find(id => id == model)));
       brandsFilter.forEach(elem => {
-        if(listBrands.find(data => data.id === elem.id) === undefined) {
-          if (elem.state == 0) {
-            listBrands.push(elem)
-          } else {
-            listBrands.push(elem)
-          }
+        if (listBrands.find(data => data.id === elem.id) === undefined) {
+          listBrands.push(elem)
         }
       });
 
       /****************************sizes*****************************/
-      const sizesFilter = this.listSizes.filter(elem => {
-        if(elem.belongsModels.find(elem => elem === filterUsed.id)){
-          return elem
-        }
-      });
+      const sizesFilter = this.listSizes.filter(elem => !!elem.belongsModels.find(model => !!filterUsed.available_ids.find(id => id == model)));
       sizesFilter.forEach(elem => {
-        if(listSizes.find(data => data.id === elem.id) === undefined) {
-          if (elem.state == 0) {
-            listSizes.push(elem)
-          } else {
-            listSizes.push(elem)
-          }
+        if (listSizes.find(data => data.id === elem.id) === undefined) {
+          listSizes.push(elem)
         }
       });
+
       /*****************************color****************************/
-      const colorFilter = this.listColors.filter(elem => {
-        if(elem.belongsModels.find(elem => elem === filterUsed.id)){
-          return elem
-        }
-      });
+      const colorFilter = this.listColors.filter(elem => !!elem.belongsModels.find(model => !!filterUsed.available_ids.find(id => id == model)));
       colorFilter.forEach(elem => {
-        if(listColors.find(data => data.id === elem.id) === undefined) {
-          if (elem.state == 0) {
-            listColors.push(elem)
-          } else {
-            listColors.push(elem)
-          }
+        if (listColors.find(data => data.id === elem.id) === undefined) {
+          listColors.push(elem)
         }
       })
     }
@@ -299,6 +260,11 @@ export class ManualReceptionComponent implements OnInit, OnDestroy {
       this.listModels = listModels;
       if (!this.modelSelected && this.listModels.length == 1) {
         this.modelSelected = this.listModels[0];
+        this.modelIdSelected =  this.modelSelected.id;
+
+        if (typeFilter == 'Colores') {
+          this.modelIdSelected = this.modelSelected.available_ids.find(id => !!this.colorSelected.belongsModels.find(model => model == id));
+        }
       }
     }
     if (listBrands.length > 0) {
