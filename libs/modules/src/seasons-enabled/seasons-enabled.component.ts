5f9d49013312729be2a13f5c44ab3520c9fbdd49
bruno-galvintec <bruno.galvintec@gmail.com>
2020-03-10 12:43:09 +0100

    List of seasons in SGA and indicate if they are enabled to receive goods.

--- libs/modules/src/seasons-enabled/seasons-enabled.component.ts
+++ libs/modules/src/seasons-enabled/seasons-enabled.component.ts
@@ -1,14 +1,11 @@
 import { BehaviorSubject, of, Observable } from 'rxjs';
 import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { MatPaginator, MatTableDataSource, MatSort, MatCheckboxChange, Sort } from '@angular/material';
-import * as Filesave from 'file-saver';
 import {
-  ProductModel,
   ProductsService,
   FiltersService,
   FiltersModel,
   InventoryService,
-  InventoryModel,
   TypesService,
   WarehouseService,
   WarehousesService,
@@ -16,22 +13,18 @@ import {
   UsersService,
   PermissionsModel,
 } from '@suite/services';
-import { HttpResponse } from '@angular/common/http';
 import { FormBuilder, FormGroup, FormControl, FormArray } from '@angular/forms';
 import { ModalController, AlertController } from '@ionic/angular';
-import { validators } from '../utils/validators';
+
 import { PrinterService } from 'libs/services/src/lib/printer/printer.service';
 import { TagsInputOption } from '../components/tags-input/models/tags-input-option.model';
 import { PaginatorComponent } from '../components/paginator/paginator.component';
 import { FilterButtonComponent } from "../components/filter-button/filter-button.component";
-import { map, catchError, filter } from 'rxjs/operators';
+
 import { PermissionsService } from '../../../services/src/lib/endpoint/permissions/permissions.service';
 import { SeasonsEnabledService } from '../../../services/src/lib/endpoint/seasons-enabled/seasons-enabled.service';
-import { PredistributionModel } from '../../../services/src/models/endpoints/Predistribution';
-import Predistribution = PredistributionModel.Predistribution;
-import { SelectionModel } from '@angular/cdk/collections';
-import { PredistributionsService } from '../../../services/src/lib/endpoint/predistributions/predistributions.service';
-import * as _ from 'lodash';
+
+
 import { parse } from 'querystring';
 
 
@@ -43,22 +36,20 @@ import { parse } from 'querystring';
   styleUrls: ['./seasons-enabled.component.scss']
 })
 
-export class SeasonsEnabledComponent implements OnInit, AfterViewInit {
+export class SeasonsEnabledComponent implements OnInit {
+
   @ViewChild(PaginatorComponent) paginator: PaginatorComponent;
   @ViewChild(MatSort) sort: MatSort;
+
   displayedColumns: string[] = ['Temporada', 'Habilitada'];
-  // displayedColumns: string[] = [];
   columns = {};
-  dataSourceOriginal;
   dataSource;
   toUpdate: FormGroup = this.formBuilder.group({
-    enableds: this.formBuilder.array([])
+    seasons: this.formBuilder.array([])
   });
-  selectionPredistribution = new SelectionModel<Predistribution>(true, []);
-  selectionReserved = new SelectionModel<Predistribution>(true, []);
+
   @ViewChild('filterButtonSeasons') filterButtonSeasons: FilterButtonComponent;
   @ViewChild('filterButtonEnableds') filterButtonEnableds: FilterButtonComponent;
-
 
   isFilteringSeasons: number = 0;
   isFilteringEnableds: number = 0;
@@ -67,16 +58,20 @@ export class SeasonsEnabledComponent implements OnInit, AfterViewInit {
   seasons: Array<TagsInputOption> = [];
   enableds: Array<TagsInputOption> = [];
 
+  /** Filters save **/
+  seasonsSelected: Array<any> = [];
+  enabledsSelected: Array<any> = [];
+
   groups: Array<TagsInputOption> = [];
   entities;
   pauseListenFormChange: boolean;
   lastUsedFilter: string;
-  // pagerValues = [50, 100, 1000];
-  pagerValues = [10, 20, 80];
+  pagerValues = [20, 50, 100];
   currentPageFilter = {
-    order:{type: '',
-    direction:''
-  }
+    order:{
+      type: '',
+      direction:''
+    }
   };
   form: FormGroup = this.formBuilder.group({
     seasons: [],
@@ -86,17 +81,10 @@ export class SeasonsEnabledComponent implements OnInit, AfterViewInit {
       limit: this.pagerValues[0]
     }),
     orderby: this.formBuilder.group({
-      type: '3',
-      order: "ASC"
+      type: '2',
+      order: "DESC"
     })
   });
-  length: any;
-
-  isShow = false;
-  seconds:any = {
-    GlobalVariable_value: ''
-  };
-
 
   constructor(
     private intermediaryService: IntermediaryService,
@@ -112,63 +100,8 @@ export class SeasonsEnabledComponent implements OnInit, AfterViewInit {
     private printerService: PrinterService,
     private usersService: UsersService,
     private permisionService: PermissionsService,
-    private seasonsEnabledService: SeasonsEnabledService,
-    private predistributionsService: PredistributionsService
+    private seasonsEnabledService: SeasonsEnabledService
   ) {
-  }
-
-  getSecondsAvelon(){
-    this.intermediaryService.presentLoading('Actualizando Avelon').then(() => {
-      this.seasonsEnabledService.GetSecondAvelon().subscribe(result => {
-        let seconds = parseInt(result.GlobalVariable_value)/60
-        seconds.toString();
-        this.seconds.GlobalVariable_value = seconds ;
-        this.intermediaryService.dismissLoading();
-        this.intermediaryService.presentToastSuccess("Actualizacion exitosa.")
-      },()=>{
-        this.intermediaryService.dismissLoading();
-        this.intermediaryService.presentToastError("Actualizacion Fallida.");
-      });
-    });
-  }
-
-  notifyAvelonPredistribution(){
-    this.intermediaryService.presentLoading('Notificando a avelon').then(() => {
-      let body = {
-        "force": true
-      };
-      this.seasonsEnabledService.notifyAvelonPredistribution(body).subscribe(result => {
-        this.intermediaryService.dismissLoading();
-        this.intermediaryService.presentToastSuccess("Notificacion enviada con exito.")
-      },()=>{
-        this.intermediaryService.dismissLoading();
-        this.intermediaryService.presentToastError("Notifiacion fallida.");
-      });
-    });
-  }
-
-
-  insertSecond(){
-    this.intermediaryService.presentLoading('Actualizando tiempo de avelon').then(() => {
-      let value = parseInt(this.seconds.GlobalVariable_value)*60;
-      value.toString();
-      let body = {
-        "value": value
-      };
-      this.seasonsEnabledService.updateSecondAvelon(body).subscribe(result => {
-        this.intermediaryService.dismissLoading();
-        this.isShow = false;
-        this.intermediaryService.presentToastSuccess("Actualizacion realizada con exito.")
-      },()=>{
-        this.intermediaryService.dismissLoading();
-        this.intermediaryService.presentToastError("Actualizacion fallida.");
-      });
-    });
-  }
-
-
-  showForm(){
-    this.isShow = true;
   }
 
   ngOnInit(){
@@ -177,12 +110,14 @@ export class SeasonsEnabledComponent implements OnInit, AfterViewInit {
     this.getFilters()
     this.getList(this.form)
     this.listenChanges()
-    this.getSecondsAvelon();
+    this.lastUsedFilter = '';
   }
+
   listenChanges() {
     let previousPageSize = this.form.value.pagination.limit;
     /**detect changes in the paginator */
     this.paginator.page.subscribe(page => {
+      this.saveFilters();
       /**true if only change the number of results */
       let flag = previousPageSize === page.pageSize;
       previousPageSize = page.pageSize;
@@ -190,14 +125,15 @@ export class SeasonsEnabledComponent implements OnInit, AfterViewInit {
         limit: page.pageSize,
         page: flag ? page.pageIndex : 1
       });
+      this.recoverFilters();
       this.getList(this.form)
     });
     this.sort.sortChange.subscribe((sort: Sort) => {
       this.intermediaryService.presentLoading('Cargando Filtros...').then(() => {
         if (sort.direction == '') {
         this.form.get("orderby").patchValue({
-          type: '1',
-          order: "ASC"
+          type: '2',
+          order: "DESC"
         });
       } else {
         this.form.get("orderby").patchValue({
@@ -208,10 +144,8 @@ export class SeasonsEnabledComponent implements OnInit, AfterViewInit {
         this.getList(this.form);
       });
     });
-    this.intermediaryService.presentLoading('Cargando Filtros...').then(() => {
-      this.getList(this.form);
-    });
   }
+
   initEntity() {
     this.entities = {
       seasons: [],
@@ -219,6 +153,7 @@ export class SeasonsEnabledComponent implements OnInit, AfterViewInit {
       ordertypes: [],
     }
   }
+
   initForm() {
     this.form.patchValue({
       seasons: [],
@@ -227,129 +162,16 @@ export class SeasonsEnabledComponent implements OnInit, AfterViewInit {
     })
   }
 
-
-  ngAfterViewInit(): void {
-    let This = this;
-    console.log(this.dataSource);
-    // setTimeout(() => {
-    //   if(!!This.sort && !!this.dataSource)
-    //     this.dataSource.sort = This.sort;
-    //   if(!!This.paginator && !!this.dataSource)
-    //     this.dataSource.paginator = This.paginator;
-    // }, 2000)
+  private saveFilters(){
+    this.seasonsSelected = this.form.value.seasons;
+    this.enabledsSelected = this.form.value.enableds;
   }
 
-  getRangeLabel = (page: number, pageSize: number, length: number) =>  {
-    if (length === 0 || pageSize === 0) {
-      return `0 / ${length}`;
-    }
-    length = Math.max(length, 0);
-    return `${length} resultados / pág. ${page + 1} de ${Math.ceil(length / pageSize)}`;
-  };
-
-  isAllSelectedPredistribution() {
-    if (this.dataSource) {
-      const numSelected = this.selectionPredistribution.selected.length;
-      const numRows = this.dataSource.data.length;
-      return numSelected === numRows;
-    }
-    return false
+  private recoverFilters(){
+    this.form.get("seasons").patchValue(this.seasonsSelected, { emitEvent: false });
+    this.form.get("enableds").patchValue(this.enabledsSelected, { emitEvent: false });
   }
 
-  isAllSelectedReserved() {
-    if (this.dataSource) {
-      const numSelected = this.selectionReserved.selected.length;
-      const numRows = this.dataSource.data.length;
-      return numSelected === numRows;
-    }
-    return false
-  }
-
-  predistributionToggle() {
-    if (this.isAllSelectedPredistribution()) {
-      this.dataSource.data.forEach(row => {
-        row.distribution = false;
-      });
-
-      this.selectionPredistribution.clear()
-    } else {
-      this.dataSource.data.forEach(row => {
-        row.distribution = true;
-        this.selectionPredistribution.select(row);
-      });
-    }
-  }
-
-  reservedToggle() {
-    if (this.isAllSelectedReserved()) {
-      this.dataSource.data.forEach(row => {
-        row.reserved = false;
-      });
-      this.selectionReserved.clear();
-    } else {
-      this.dataSource.data.forEach(row => {
-        row.reserved = true;
-        this.selectionReserved.select(row);
-      });
-    }
-  }
-
-  checkboxLabelPredistribution(row?): string {
-    if (!row) {
-      return `${this.isAllSelectedPredistribution() ? 'select' : 'deselect'} all`;
-    }
-    return `${this.selectionPredistribution.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
-  }
-
-  checkboxLabelReserved(row?): string {
-    if (!row) {
-      return `${this.isAllSelectedReserved() ? 'select' : 'deselect'} all`;
-    }
-    return `${this.selectionReserved.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
-  }
-
-  changePredistribution(row, position: number) {
-    this.dataSource.data[position].distribution = !this.dataSource.data[position].distribution;
-  }
-
-  changeReserved(row, position: number) {
-    this.dataSource.data[position].reserved = !this.dataSource.data[position].reserved;
-  }
-
-  async savePredistributions() {
-    let list = [];
-
-    this.dataSource.data.forEach((dataRow, index) => {
-      if (this.dataSourceOriginal.data[index].distribution !== dataRow.distribution ||
-        this.dataSourceOriginal.data[index].reserved !== dataRow.reserved) {
-        list.push({
-          distribution: !!dataRow.distribution,
-          reserved: !!dataRow.reserved,
-          modelId: dataRow.model.id,
-          sizeId: dataRow.size.id,
-          warehouseId: dataRow.warehouse.id,
-          expeditionLineId: dataRow.expeditionLineId
-        })
-      }
-    });
-
-
-    this.intermediaryService.presentLoading();
-
-    await this.predistributionsService.updateBlockReserved(list).subscribe((data) => {
-      this.intermediaryService.presentToastSuccess("Actualizado predistribuciones correctamente");
-      this.initEntity();
-      this.initForm();
-      this.getFilters();
-      this.getList(this.form);
-      this.listenChanges();
-    }, (error) => {
-      this.intermediaryService.presentToastError("Error Actualizado predistribuciones");
-      this.intermediaryService.dismissLoading();
-    }, () => {
-      this.intermediaryService.dismissLoading();
-    });
-  }
   getFilters() {
     this.seasonsEnabledService.entities().subscribe(entities => {
       entities.ordertypes.forEach(element => {
@@ -362,35 +184,30 @@ export class SeasonsEnabledComponent implements OnInit, AfterViewInit {
       setTimeout(() => {
         this.pauseListenFormChange = false;
         this.pauseListenFormChange = true;
-        // this.form.get("warehouses").patchValue([warehouse.id], { emitEvent: false });
-        // this.form.get("orderby").get("type").patchValue("" + TypesService.ID_TYPE_ORDER_PRODUCT_DEFAULT, { emitEvent: false });
       }, 0);
     })
-
   }
+
   async getList(form?: FormGroup){
     await this.intermediaryService.presentLoading();
     this.seasonsEnabledService.index(form.value).subscribe(
       (resp:any) => {
+        this.toUpdate.removeControl("seasons");
+        this.toUpdate.addControl("seasons", this.formBuilder.array(resp.results.map(season => {
+          return this.formBuilder.group({
+            id: season.id,
+            name: season.name,
+            selected: season.reception && season.reception.enabled ? season.reception.enabled : false,
+            seasonReception: season.reception
+          });
+        })));
         this.dataSource = new MatTableDataSource<any>(resp.results);
         const paginator = resp.pagination;
 
         this.paginator.length = paginator.totalResults;
         this.paginator.pageIndex = paginator.selectPage;
         this.paginator.lastPage = paginator.lastPage;
-        this.selectionPredistribution.clear();
-        this.selectionReserved.clear();
 
-        this.dataSource.data.forEach(row => {
-        if (row.distribution) {
-          this.selectionPredistribution.select(row);
-        }
-        if (row.reserved) {
-           this.selectionReserved.select(row);
-        }
-
-        this.dataSourceOriginal = _.cloneDeep(this.dataSource)
-       });
       },
       async err => {
         await this.intermediaryService.dismissLoading()
@@ -407,17 +224,17 @@ export class SeasonsEnabledComponent implements OnInit, AfterViewInit {
       case 'seasons':
         let seasonsFiltered: string[] = [];
         for (let season of filters) {
-          if (season.checked) seasonsFiltered.push(season.name);
+          if (season.checked) seasonsFiltered.push(season.id);
         }
         if (seasonsFiltered.length >= this.seasons.length) {
-          this.form.value.productReferencePattern = [];
+          this.form.value.seasons = [];
           this.isFilteringSeasons = this.seasons.length;
         } else {
           if (seasonsFiltered.length > 0) {
-            this.form.value.productReferencePattern = seasonsFiltered;
+            this.form.value.seasons = seasonsFiltered;
             this.isFilteringSeasons = seasonsFiltered.length;
           } else {
-            this.form.value.productReferencePattern = [];
+            this.form.value.seasons = [];
             this.isFilteringSeasons = this.seasons.length;
           }
         }
@@ -483,9 +300,14 @@ export class SeasonsEnabledComponent implements OnInit, AfterViewInit {
     this.pauseListenFormChange = true;
     let value = this.form.get("enableds").value;
     this.enableds = enableds.map(enabled => {
-      enabled.id = <number>(<unknown>enabled.reference);
-      enabled.name = enabled.reference;
-      enabled.value = enabled.name;
+      if(enabled.name == "1"){
+        enabled.name = "1";
+        enabled.value = "Si";
+      }else{
+        enabled.id = 0;
+        enabled.name = "0";
+        enabled.value = "No";
+      }
       enabled.checked = true;
       enabled.hide = false;
       return enabled;
@@ -505,53 +327,23 @@ export class SeasonsEnabledComponent implements OnInit, AfterViewInit {
     setTimeout(() => { this.pauseListenFormChange = false; }, 0);
   }
 
-  // new
-  public changeStatusBlocked( event:MatCheckboxChange, row) {
-    this.dataSource.data.forEach(function(value){
-      if(value.expeditionLineId === row.expeditionLineId) {
-        value.distribution = event.checked;
+  update() {
+    let observable = new Observable(observer => observer.next());
+    this.seasonsEnabledService.updateSeasons(this.toUpdate.value).subscribe(async result => {});
+    this.intermediaryService.presentLoading();
+    observable.subscribe(
+      () => {
+        this.intermediaryService.dismissLoading();
+        this.intermediaryService.presentToastSuccess(
+          'Temporada actualizada con exito'
+        );
+        this.getList(this.form);
+      },
+      () => {
+        this.intermediaryService.dismissLoading();
+        this.intermediaryService.presentToastSuccess('Error actualizando temporada');
       }
-    });
-  }
-  public  isCheckedStatusBlocked( element) {
-    return element.distribution;
-  }
-  public changeStatusBlockedAll( event:MatCheckboxChange) {
-    this.dataSource.data.forEach(function(value){
-      value.distribution = event.checked;
-    });
-  }
-  public  isCheckedStatusBlockedAll() {
-    let result = true;
-    this.dataSource.data.forEach(function(value){
-      result = result && value.distribution;
-    });
-    return result;
-  }
-  // reserved
-  public changeStatusReserved(event:MatCheckboxChange, row) {
-    this.dataSource.data.forEach(function(value){
-      if(value.expeditionLineId === row.expeditionLineId) {
-        value.distribution = !event.checked;
-      }
-    });
-  }
-
-  public isCheckedStatusReserved( element) {
-    return !element.distribution;
-  }
-
-  public changeStatusReservedAll( event:MatCheckboxChange) {
-    this.dataSource.data.forEach(function(value){
-      value.distribution = !event.checked;
-    });
-  }
-  public  isCheckedStatusReservedAll() {
-    let result = true;
-    this.dataSource.data.forEach(function(value){
-      result = result && !value.distribution;
-    });
-    return result;
+    );
   }
 
   refreshTable() {
--- libs/services/src/lib/endpoint/seasons-enabled/seasons-enabled.service.ts
+++ libs/services/src/lib/endpoint/seasons-enabled/seasons-enabled.service.ts
@@ -2,14 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 
 import { Observable } from 'rxjs/internal/Observable';
-import { AuthenticationService } from '../authentication/authentication.service';
-
-import { ProductModel } from '../../../models/endpoints/Product';
 
 import { environment } from '../../../environments/environment';
 import {map, switchMap} from 'rxjs/operators';
-import { from, observable } from 'rxjs';
-import {RequestsProvider} from "../../../providers/requests/requests.provider";
 import {HttpRequestModel} from "../../../models/endpoints/HttpRequest";
 
 @Injectable({
@@ -18,55 +13,25 @@ import {HttpRequestModel} from "../../../models/endpoints/HttpRequest";
 
 export class SeasonsEnabledService {
   /**routes for services */
-  private getSecondAvelon = environment.apiBase+'/global-variables/GetSecondsToAvelon';
-  private notifyAvelon = environment.apiBase+'/reception/notify-avelon-predistribution';
-  private getProducts = environment.apiBase+'/products/';
-  private UpdateSecondAvelon = environment.apiBase+'/global-variables/SecondsToAvelon';
-  private indexRoute = environment.apiBase+'/reception/notify-avelon/search';
-  private getEntities = environment.apiBase+'/reception/notify-avelon/search-filters';
   private indexUrl = environment.apiBase+'/reception/seasons/search';
-  private filtersUrl = environment.apiBase+'/reception/seasons/search-filters';
+  private entitiesUrl = environment.apiBase+'/reception/seasons/search-filters';
+  private updateUrl = environment.apiBase+'/reception/seasons/update';
 
   constructor(
-    private http: HttpClient,
-    private auth: AuthenticationService,
-    private requestsProvider: RequestsProvider
+    private http: HttpClient
   ) {}
 
-  index(body): Observable<any>{
-    return this.http.post<HttpRequestModel.Response>(this.indexRoute, body).pipe(map((response:any)=>{
-      return response.data;
-    }));
-  }
-
-  GetSecondAvelon(): Observable<any> {
-    return this.http.get(this.getSecondAvelon).pipe(map((response:any)=>{
-      return response.data;
-    }));
-  }
-
-  notifyAvelonPredistribution(body): Observable<any>{
-    return this.http.post(this.notifyAvelon, body).pipe(map((data:any)=>{
-      return data.data;
-    }));
-  }
-
-  GetProducts(): Observable<any>{
-    return this.http.get(this.getProducts).pipe(map((data:any)=>{
-      return data.data;
-    }));
-  }
-
-
-  updateSecondAvelon(body): Observable<any>{
-    return this.http.post(this.UpdateSecondAvelon, body).pipe(map((data:any)=>{
-      return data.data;
-    }));
+  index(body): Observable<any> {
+    return this.http.post<HttpRequestModel.Response>(this.indexUrl,body).pipe(map(resp => resp.data))
   }
 
   entities(): Observable<any>{
-    return this.http.get(this.getEntities).pipe(map((data:any)=>{
+    return this.http.get(this.entitiesUrl).pipe(map((data:any)=>{
       return data.data.filters;
     }));
+  }
+
+  updateSeasons(body): Observable<any>{
+    return this.http.post<HttpRequestModel.Response>(this.updateUrl,body).pipe(map(resp => resp.data))
   }
 }
