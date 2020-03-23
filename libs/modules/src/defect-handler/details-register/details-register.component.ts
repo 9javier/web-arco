5b5a50906c1c0a590fb116d0edc9a279b4848d2b
Luis Álvarez <luis.alvarez@galvintec.com>
2020-03-23 09:23:21 +0100

    Fixes of undefined variables in defect management

--- libs/modules/src/defect-handler/details-register/details-register.component.ts
+++ libs/modules/src/defect-handler/details-register/details-register.component.ts
@@ -133,21 +133,24 @@ export class DetailsRegisterComponent implements OnInit {
   }
 
   getRequireStatus(defectType: number, statusName: string) {
-    const status = this.statusManagement.classifications.find((x) => x.defectType === defectType);
-
-    switch (statusName) {
-      case 'contact':
-        return status.requireContact;
-      case 'history':
-        return status.passHistory;
-      case 'photo':
-        return status.requirePhoto;
-      case 'signature':
-        return status.requireOk;
-      case 'ticket':
-        return status.ticketEmit;
-      case 'orders':
-        return status.allowOrders;
+    if(this.statusManagement && this.statusManagement.classifications){
+      const status = this.statusManagement.classifications.find((x) => defectType && x.defectType === defectType);
+      if(status){
+        switch (statusName) {
+          case 'contact':
+            return status.requireContact;
+          case 'history':
+            return status.passHistory;
+          case 'photo':
+            return status.requirePhoto;
+          case 'signature':
+            return status.requireOk;
+          case 'ticket':
+            return status.ticketEmit;
+          case 'orders':
+            return status.allowOrders;
+        }
+      }
     }
   }
 }
