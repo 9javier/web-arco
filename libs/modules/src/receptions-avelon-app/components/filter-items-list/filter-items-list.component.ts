0b4533557db584561434ee60d282bd75b4fe26ee
Marcelo <andresmdelrioraiz@gmail.com>
2020-04-16 19:04:33 +0200

    Change system to optimize load of items for reception manual in AL to avoid empty lists.

--- libs/modules/src/receptions-avelon-app/components/filter-items-list/filter-items-list.component.ts
+++ libs/modules/src/receptions-avelon-app/components/filter-items-list/filter-items-list.component.ts
@@ -1,5 +1,5 @@
-import {Component, OnInit} from '@angular/core';
-import {ModalController} from "@ionic/angular";
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {ModalController, IonInfiniteScroll} from "@ionic/angular";
 
 @Component({
   selector: 'suite-filter-items-list',
@@ -8,18 +8,24 @@ import {ModalController} from "@ionic/angular";
 })
 export class FilterItemsListComponent implements OnInit {
 
+  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
+
   public filterListType: string = '';
   public listItems: {id: number, name: string, color: string}[] = [];
   public searchText: string = null;
 
   private originaListItems = [];
+  private paginatedListItems = [];
+
+  private LIMIT_PAGINATION: number = 30;
 
   constructor(
     private modalController: ModalController
   ) { }
 
   ngOnInit() {
-    this.originaListItems = JSON.parse(JSON.stringify(this.listItems.sort((a, b) => a.name.trim().localeCompare(b.name.trim()))));
+    this.originaListItems = this.clone(this.listItems.sort((a, b) => a.name.trim().localeCompare(b.name.trim())));
+    this.paginatedListItems = (this.clone(this.listItems.sort((a, b) => a.name.trim().localeCompare(b.name.trim())))).splice(0, this.LIMIT_PAGINATION);
   }
 
   public close() {
@@ -31,17 +37,32 @@ export class FilterItemsListComponent implements OnInit {
   }
 
   public changeSearchData() {
+    this.paginatedListItems = [];
     if (this.searchText) {
-      this.listItems = this.originaListItems.filter(i => {
+      this.listItems = this.clone(this.originaListItems.filter(i => {
         return i.name.toUpperCase().includes(this.searchText.toUpperCase());
-      });
+      }));
     } else {
-      this.listItems = JSON.parse(JSON.stringify(this.originaListItems));
+      this.listItems = this.clone(this.originaListItems);
     }
+    this.paginatedListItems = (this.clone(this.listItems)).splice(0, this.LIMIT_PAGINATION);
+    this.infiniteScroll.disabled = false;
   }
 
   public resetSearchBar() {
     this.searchText = null;
     this.changeSearchData();
+  }
+
+  public loadData(event) {
+    event.target.complete();
+    this.paginatedListItems = this.paginatedListItems.concat((this.clone(this.listItems)).splice(this.paginatedListItems.length, this.LIMIT_PAGINATION));
+    if (this.paginatedListItems.length == this.listItems.length) {
+      event.target.disabled = true;
+    }
+  }
+
+  private clone(toClone: any) {
+    return JSON.parse(JSON.stringify(toClone));
   }
 }
