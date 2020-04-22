d7e261db58da4fcad34ebb506909aef6b98f2c6a
bruno-galvintec <bruno.galvintec@gmail.com>
2020-04-22 17:42:28 +0200

    Filters in the list of products ordered after reception in the shop.

--- libs/services/src/models/endpoints/AppFilters.ts
+++ libs/services/src/models/endpoints/AppFilters.ts
@@ -10,11 +10,30 @@ export namespace AppFiltersModel {
     ordertypes: any
   }
 
+  export interface ProductsRequested {
+    models: any,
+    brands: any,
+    references: any,
+    sizes: any,
+    dates: any,
+    lifestyles: any,
+    families: any,
+    ordertypes: any
+  }
+
   export interface ParamsProductsReceived {
     packingId?: number
   }
 
+  export interface ParamsProductsRequested {
+    warehouseId?: number
+  }
+
   export interface ResponseProductsReceived {
     data: ProductsReceived
+  }
+
+  export interface ResponseProductsRequested {
+    data: ProductsRequested
   }
 }
