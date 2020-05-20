2fe2f9331353301569eeea1237ebcb193b971c72
Marcelo <andresmdelrioraiz@gmail.com>
2020-05-20 10:54:00 +0200

    Improve design for list of returns.

--- libs/modules/src/return-tracking-list/return-tracking-list.module.ts
+++ libs/modules/src/return-tracking-list/return-tracking-list.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from "@angular/router";
-import { MatPaginatorModule, MatTooltipModule } from "@angular/material";
+import { MatPaginatorModule, MatTooltipModule, MatTableModule } from "@angular/material";
 import { IonicModule } from '@ionic/angular';
 import { ReturnTrackingListComponent } from './return-tracking-list.component';
 import { FilterButtonModule } from "../components/filter-button/filter-button.module";
@@ -19,6 +19,7 @@ const routes: Routes = [{
     RouterModule.forChild(routes),
     MatPaginatorModule,
     MatTooltipModule,
+    MatTableModule,
     FilterButtonModule
   ]
 })
