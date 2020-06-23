0ec12621d3739744f86b3caca9eb358caa26f9ac
Áxel <axel@galvintec.com>
2020-06-23 18:21:11 +0200

    Add compile processes memory limit for dev front server
    Solves permanent error `Ineffective mark-compacts near heap limit Allocation failed - JavaScript heap out of memory`
    Makes compile very slow - about 15 min
    Not sure if this is necessary after downgrading the server RAM, after changing the compile options to a production-like set, or another reason

--- _dev/web/update_sga_dev.sh
+++ _dev/web/update_sga_dev.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 #su
 #su sga
+export NODE_OPTIONS="--max-old-space-size=1900"
 cd /home/sga/sga-app-logistica-krack/ && git reset --hard && git pull && ionic build --project sga --configuration=dev --prod -- --base-href /sga/ && rm -rf ../content && cp -R dist/apps ../content && cp ../content/sga/index.html ../content/index.html
